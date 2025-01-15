03c13312ed85bf5286a7afa4b521ed7041c31c5e
Vladimir Lazar <vlazar@redhat.com>
2025-01-15 17:33:46 +0200

    chore(test): maintain check when not in gha cicd Signed-off-by: Vladimir Lazar <vlazar@redhat.com>

--- tests/playwright/src/specs/podman-compose-smoke.spec.ts
+++ tests/playwright/src/specs/podman-compose-smoke.spec.ts
@@ -19,10 +19,16 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+import { CLIToolsPage } from '../model/pages/cli-tools-page';
+import { ResourceCliCardPage } from '../model/pages/resource-cli-card-page';
+import { ResourcesPage } from '../model/pages/resources-page';
+import { SettingsBar } from '../model/pages/settings-bar';
 import { expect as playExpect, test } from '../utility/fixtures';
 import { deleteContainer, deleteImage, runComposeUpFromCLI } from '../utility/operations';
+import { isCI, isLinux } from '../utility/platform';
 import { waitForPodmanMachineStartup } from '../utility/wait';
 
+const RESOURCE_NAME: string = 'Compose';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const backendContainerName = 'backend-1';
@@ -50,6 +56,23 @@ test.afterAll(async ({ page, runner }) => {
 });
 
 test.describe.serial('Compose compose workflow verification', { tag: '@smoke' }, () => {
+  test('Verify Compose was installed', async ({ page, navigationBar }) => {
+    test.skip(!!isCI && isLinux, 'This test should not run on Ubuntu platform in Github Actions');
+
+    await navigationBar.openSettings();
+    const settingsBar = new SettingsBar(page);
+    const resourcesPage = await settingsBar.openTabPage(ResourcesPage);
+    await playExpect.poll(async () => await resourcesPage.resourceCardIsVisible(RESOURCE_NAME)).toBeTruthy();
+    const composeBox = new ResourceCliCardPage(page, RESOURCE_NAME);
+    const setupButton = composeBox.setupButton;
+    await playExpect(setupButton).toBeHidden();
+
+    const cliToolsPage = await settingsBar.openTabPage(CLIToolsPage);
+    const composeRow = cliToolsPage.toolsTable.getByLabel(RESOURCE_NAME);
+    const composeVersionInfo = composeRow.getByLabel('cli-version');
+    await playExpect(composeVersionInfo).toContainText('docker-compose');
+  });
+
   test('Check Podman Desktop autorefresh when using podman compose up', async ({ navigationBar }) => {
     test.setTimeout(300_000);
 
