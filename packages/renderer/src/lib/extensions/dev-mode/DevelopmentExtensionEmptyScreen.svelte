8b76990095c3dea65f26c4e1dafa23e32f434726
Florent Benoit <fbenoit@redhat.com>
2025-01-20 14:40:20 +0100

    feat: allow to use production binary to develop extensions
    fixes https://github.com/podman-desktop/podman-desktop/issues/8616
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ packages/renderer/src/lib/extensions/dev-mode/DevelopmentExtensionEmptyScreen.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { faCog } from '@fortawesome/free-solid-svg-icons';
+import { Button, EmptyScreen } from '@podman-desktop/ui-svelte';
+
+async function openExtensionDocumentation(): Promise<void> {
+  await window.openExternal('https://podman-desktop.io/docs/extensions/developing');
+}
+</script>
+
+<EmptyScreen
+  icon={faCog}
+  title="Usage of local extensions (developer mode)"
+  message="Please enable development mode feature in the preferences">
+  <div class="flex gap-2 justify-center">
+    <Button type="link" on:click={openExtensionDocumentation}>Documentation: How to write your first extension</Button>
+  </div>
+</EmptyScreen>
