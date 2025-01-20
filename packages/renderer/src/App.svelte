9b248f6c6d111a0a2fba64e8acff2571896d78cc
Sonia Sandler <ssandler@redhat.com>
2025-01-19 22:26:36 -0500

    chore: remove console logs Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/renderer/src/App.svelte
+++ packages/renderer/src/App.svelte
@@ -157,7 +157,6 @@ window.events?.receive('kubernetes-navigation', (args: unknown) => {
         </Route>
         <Route path="/containers/:id/*" let:meta firstmatch>
           <Route path="/create-container-existing-image" breadcrumb="Select image" >
-            {console.log('create container')}
             <CreateContainerFromExisitingImage />
           </Route>
           <Route path="/export" breadcrumb="Export Container">
