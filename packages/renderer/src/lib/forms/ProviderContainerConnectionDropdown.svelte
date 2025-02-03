5d6971375a34758493315654e97865e50ee1b2ff
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-02-03 15:29:11 +0100

    refactor(dropdown): create container connections dropdown component
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

---
+++ packages/renderer/src/lib/forms/ProviderContainerConnectionDropdown.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+import { Dropdown } from '@podman-desktop/ui-svelte';
+
+import type { ProviderContainerConnectionInfo } from '/@api/provider-info';
+
+interface Props {
+  connections: ProviderContainerConnectionInfo[];
+  value: ProviderContainerConnectionInfo | undefined;
+  name?: string;
+  id?: string;
+  onchange?(value: ProviderContainerConnectionInfo | undefined): void;
+  class?: string;
+  disabled?: boolean;
+}
+
+let {
+  connections,
+  name = 'providerChoice',
+  id,
+  value = $bindable(),
+  onchange,
+  class: className,
+  disabled,
+}: Props = $props();
+
+function handleChange(nValue: string | undefined): void {
+  if (nValue) {
+    value = connections.find(connection => connection.endpoint.socketPath === nValue);
+  } else {
+    value = undefined;
+  }
+
+  onchange?.(value);
+}
+</script>
+
+<Dropdown
+  id={id}
+  class={className}
+  name={name}
+  disabled={disabled}
+  value={value?.endpoint?.socketPath}
+  onChange={handleChange}
+  options={connections.map(providerConnection => ({
+            label: providerConnection.name,
+            value: providerConnection.endpoint.socketPath,
+          }))}>
+</Dropdown>
