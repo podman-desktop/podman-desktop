fd031a839d9b6f064912fcc2d84358a790dc6b52
Vladimir Lazar <vlazar@redhat.com>
2025-02-04 10:44:06 +0100

    chore(test): bracket in the wrong place Signed-off-by: Vladimir Lazar <vlazar@redhat.com>

--- tests/playwright/src/utility/operations.ts
+++ tests/playwright/src/utility/operations.ts
@@ -184,7 +184,7 @@ export async function handleConfirmationDialog(
       : dialog.getByRole('button', { name: cancelButton });
     await playExpect(button).toBeEnabled();
     await button.click();
-    await waitUntil(async () => !(await dialog.isVisible(), { timeout: 10_000 }));
+    await waitUntil(async () => !(await dialog.isVisible()), { timeout: 10_000 });
   });
 }
 
