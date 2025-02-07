6b73b6507efda7fab6862d782a1cb80f8e623bf3
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-02-07 13:24:13 +0100

    fix: removing symbol usage
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/forms/ContainerConnectionDropdown.svelte
+++ packages/renderer/src/lib/forms/ContainerConnectionDropdown.svelte
@@ -23,18 +23,22 @@ let {
   disabled,
 }: Props = $props();
 
+let selected: string | undefined = $state<string>();
+
 /**
- * Internally this component generate a symbol for each connection provided
+ * Internally this component generate a unique string for each connection provided
  * This ensure we have a unique identifier
  */
-let items: Map<symbol, ProviderContainerConnectionInfo> = $derived(
-  new Map(connections.map(connection => [Symbol(connection.name), connection])),
+let items: Map<string, ProviderContainerConnectionInfo> = $derived(
+  new Map(connections.map(connection => [`${connection.type}:${connection.name}`, connection])),
 );
 
 function handleChange(nValue: unknown): void {
-  if (typeof nValue === 'symbol') {
+  if (typeof nValue === 'string') {
+    selected = nValue;
     value = items.get(nValue);
   } else {
+    selected = undefined;
     value = undefined;
   }
 
@@ -47,7 +51,7 @@ function handleChange(nValue: unknown): void {
   class={className}
   name={name}
   disabled={disabled}
-  value={value?.endpoint?.socketPath}
+  value={selected}
   onChange={handleChange}
   options={Array.from(items.entries()).map(([key, connection]) => ({
             label: connection.name,
