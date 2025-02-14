052fd52da3c365ddbc011688277289355ad91302
Daniel Villanueva <davillan@redhat.com>
2025-02-14 10:29:45 +0100

    chore(test): fix counting rows tables
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>

--- tests/playwright/src/special-specs/ui-stress/ui-stress.spec.ts
+++ tests/playwright/src/special-specs/ui-stress/ui-stress.spec.ts
@@ -47,7 +47,7 @@ test.describe.serial('Verification of UI handling lots of objects', { tag: ['@ui
     test.setTimeout(30_000);
     const images = await navigationBar.openImages();
     //count images => 1 original image + (1 tagged * 10) + 1 localhost/podman-pause from pods = 12
-    await playExpect.poll(async () => images.countRowsFromTable()).toBe(12);
+    await playExpect.poll(async () => await images.countRowsFromTable(), { timeout: 10_000 }).toBe(12);
     for (let imgNum = 1; imgNum <= 10; imgNum++) {
       await playExpect
         .poll(async () => await images.waitForImageExists(`quay.io/my-image-${imgNum}`), { timeout: 5_000 })
@@ -59,7 +59,7 @@ test.describe.serial('Verification of UI handling lots of objects', { tag: ['@ui
     test.setTimeout(30_000);
     const containers = await navigationBar.openContainers();
     //count containers => (1 manually created + 2 from creating pods) * 10 = 30
-    await playExpect.poll(async () => containers.countRowsFromTable()).toBe(30);
+    await playExpect.poll(async () => await containers.countRowsFromTable(), { timeout: 10_000 }).toBe(30);
     for (let containerNum = 1; containerNum <= 10; containerNum++) {
       await playExpect
         .poll(async () => await containers.containerExists(`my-container-${containerNum}`), { timeout: 5_000 })
@@ -71,7 +71,7 @@ test.describe.serial('Verification of UI handling lots of objects', { tag: ['@ui
     test.setTimeout(30_000);
     const pods = await navigationBar.openPods();
     //count pods => 1 manually created * 10 = 10
-    await playExpect.poll(async () => pods.countRowsFromTable()).toBe(10);
+    await playExpect.poll(async () => await pods.countRowsFromTable(), { timeout: 10_000 }).toBe(10);
     for (let podNum = 1; podNum <= 10; podNum++) {
       await playExpect.poll(async () => await pods.podExists(`my-pod-${podNum}`), { timeout: 5_000 }).toBeTruthy();
     }
