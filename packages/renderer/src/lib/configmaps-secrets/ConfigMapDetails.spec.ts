bf499836f53bfc50e420916e758aef64f6f595ac
Philippe Martin <phmartin@redhat.com>
2025-02-24 18:35:40 +0100

    feat: display configmap details in experimental mode Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/renderer/src/lib/configmaps-secrets/ConfigMapDetails.spec.ts
+++ packages/renderer/src/lib/configmaps-secrets/ConfigMapDetails.spec.ts
@@ -21,13 +21,18 @@ import '@testing-library/jest-dom/vitest';
 import type { KubernetesObject, V1ConfigMap } from '@kubernetes/client-node';
 import { render, screen } from '@testing-library/svelte';
 import { writable } from 'svelte/store';
-import { beforeAll, expect, test, vi } from 'vitest';
+import { router } from 'tinro';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
 
-import * as kubeContextStore from '/@/stores/kubernetes-contexts-state';
+import * as states from '/@/stores/kubernetes-contexts-state';
+import type { IDisposable } from '/@api/disposable';
 
+import { isKubernetesExperimentalMode, listenResources } from '../kube/resources-listen';
 import ConfigMapDetails from './ConfigMapDetails.svelte';
 
 const configMap: V1ConfigMap = {
+  apiVersion: 'v1',
+  kind: 'ConfigMap',
   metadata: {
     name: 'my-configmap',
     namespace: 'default',
@@ -35,23 +40,74 @@ const configMap: V1ConfigMap = {
   data: {},
 };
 
-vi.mock('/@/stores/kubernetes-contexts-state', async () => {
+vi.mock(import('../kube/resources-listen'), async importOriginal => {
+  // we want to keep the original nonVerbose
+  const original = await importOriginal();
   return {
-    kubernetesCurrentContextConfigMaps: vi.fn(),
+    ...original,
+    listenResources: vi.fn(),
+    isKubernetesExperimentalMode: vi.fn(),
   };
 });
 
-beforeAll(() => {
-  Object.defineProperty(window, 'kubernetesReadNamespacedConfigMap', { value: vi.fn() });
+vi.mock('/@/stores/kubernetes-contexts-state');
+
+beforeEach(() => {
+  vi.resetAllMocks();
+  router.goto('http://localhost:3000');
 });
 
-test('Confirm renders configmap details', async () => {
-  // mock object store
-  const configMaps = writable<KubernetesObject[]>([configMap]);
-  vi.mocked(kubeContextStore).kubernetesCurrentContextConfigMaps = configMaps;
+type initListsReturnType = {
+  updateDeployments: (objects: KubernetesObject[]) => void;
+};
 
-  render(ConfigMapDetails, { name: 'my-configmap', namespace: 'default' });
+describe.each<{
+  experimental: boolean;
+  initLists: (configmaps: KubernetesObject[]) => initListsReturnType;
+}>([
+  {
+    experimental: false,
+    initLists: (configmaps: KubernetesObject[]): initListsReturnType => {
+      const configmapsStore = writable<KubernetesObject[]>(configmaps);
+      vi.mocked(states).kubernetesCurrentContextConfigMaps = configmapsStore;
+      return {
+        updateDeployments: (configmaps: KubernetesObject[]): void => {
+          configmapsStore.set(configmaps);
+        },
+      };
+    },
+  },
+  {
+    experimental: true,
+    initLists: (configmaps: KubernetesObject[]): initListsReturnType => {
+      let configmapsCallback: (resoures: KubernetesObject[]) => void;
+      vi.mocked(listenResources).mockImplementation(async (resourceName, _options, cb): Promise<IDisposable> => {
+        configmapsCallback = cb;
+        setTimeout(() => configmapsCallback(configmaps));
+        return {
+          dispose: (): void => {},
+        };
+      });
+      return {
+        updateDeployments: (updatedObjects: KubernetesObject[]): void => {
+          configmapsCallback(updatedObjects);
+        },
+      };
+    },
+  },
+])('is experimental: $experimental', ({ experimental, initLists }) => {
+  beforeEach(() => {
+    vi.mocked(isKubernetesExperimentalMode).mockResolvedValue(experimental);
+  });
 
-  expect(screen.getByText('my-configmap')).toBeInTheDocument();
-  expect(screen.getByText('default')).toBeInTheDocument();
+  test('Confirm renders configmap details', async () => {
+    initLists([configMap]);
+
+    render(ConfigMapDetails, { name: 'my-configmap', namespace: 'default' });
+
+    await vi.waitFor(() => {
+      expect(screen.getByText('my-configmap')).toBeInTheDocument();
+      expect(screen.getByText('default')).toBeInTheDocument();
+    });
+  });
 });
