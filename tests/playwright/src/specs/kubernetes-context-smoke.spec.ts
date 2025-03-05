da3542d2317a3c73eeee2931175b8e0358a1763f
Daniel Villanueva <davillan@redhat.com>
2025-03-05 10:00:24 +0100

    chore(test): removed unnecessary skip
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>

--- tests/playwright/src/specs/kubernetes-context-smoke.spec.ts
+++ tests/playwright/src/specs/kubernetes-context-smoke.spec.ts
@@ -46,7 +46,6 @@ test.afterAll(async ({ runner }) => {
 });
 
 test.describe.serial('Verification of kube context management', { tag: '@smoke' }, () => {
-  test.skip(process.env.ROOTFUL_MODE !== 'true', 'This test should only run on a rootful machine');
   test('Load custom kubeconfig in Preferences', async ({ navigationBar }) => {
     // open preferences page
     const settingsBar = await navigationBar.openSettings();
