fca2d6bfaa3a37d3abbb543d4d133f2c2ce0f912
Daniel Villanueva <davillan@redhat.com>
2025-03-06 13:15:14 +0100

    test(e2e): fixed linting issues
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>

--- tests/playwright/src/specs/deploy-to-kubernetes.spec.ts
+++ tests/playwright/src/specs/deploy-to-kubernetes.spec.ts
@@ -77,6 +77,7 @@ test.afterAll(async ({ runner, page }) => {
 });
 
 test.describe.serial('Deploy a container to the Kind cluster', { tag: '@k8s_e2e' }, () => {
+  console.log('todo:remove this');
   test.skip(!canRunKindTests, "This test can't run on a windows rootless machine");
   test('Pull an image and start a container', async ({ navigationBar }) => {
     const imagesPage = await navigationBar.openImages();
--- tests/playwright/src/specs/kubernetes-deployments.spec.ts
+++ tests/playwright/src/specs/kubernetes-deployments.spec.ts
@@ -90,6 +90,7 @@ test.afterAll(async ({ runner, page }) => {
 });
 
 test.describe.serial('Kubernetes deployment resource E2E Test', { tag: '@k8s_e2e' }, () => {
+  console.log('todo:remove this');
   test.skip(!canRunKindTests, "This test can't run on a windows rootless machine");
   test('Kubernetes Pods page should be empty', async ({ navigationBar }) => {
     const kubernetesBar = await navigationBar.openKubernetes();
