c33ea0be4d7b30f257dfdd8330c98cf956e6683c
Evzen Gasta <evzen.ml@seznam.cz>
2025-03-10 07:22:37 +0100

    feat: added experimental features feedback notification
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>

--- packages/renderer/src/lib/kube/KubernetesDashboard.svelte
+++ packages/renderer/src/lib/kube/KubernetesDashboard.svelte
@@ -34,6 +34,7 @@ import KubernetesDashboardResourceCard from './KubernetesDashboardResourceCard.s
 import KubernetesEmptyPage from './KubernetesEmptyPage.svelte';
 import shareYourLocalProdmanImagesWithTheKubernetesImage from './ShareYourLocalPodmanImagesWithTheKubernetes.png';
 import workingWithKubernetesImage from './WorkingWithKubernetes.png';
+import { showFeedbackDialog } from '/@/stores/feedbackForm';
 
 interface ExtendedKubernetesObject extends KubernetesObject {
   spec: {
@@ -95,6 +96,11 @@ onMount(async () => {
   disposable = await listenActiveResourcesCount((activeResourcesCounts: ResourceCount[]): void => {
     kubernetesActiveResourcesCountExperimental = activeResourcesCounts;
   });
+  // If experimental mode is enabled
+  if (isKubernetesExperimentalModeStore) {
+    // Show the feedback dialog after 3 minutes
+    setTimeout(() => showFeedbackDialog('kubernetes.statesExperimental'), 180000);
+  }
 });
 
 onDestroy(() => {
