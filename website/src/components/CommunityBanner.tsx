57339c8c810ec3a6507e3c953381e96055367c0b
Florent Benoit <fbenoit@redhat.com>
2025-03-12 19:42:54 +0100

    chore: format Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- website/src/components/CommunityBanner.tsx
+++ website/src/components/CommunityBanner.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Banner(): JSX.Element {
   return (
-    <div className="w-full flex flex-row justify-center items-center py-1 bg-gradient-to-r from-sky-700 to-purple-700 overflow-hidden px-5">
+    <div className="w-full flex flex-row justify-center items-center py-0.5 bg-gradient-to-r from-sky-700 to-purple-700 overflow-hidden px-5">
       <div className="mx-3 relative text-white font-semibold">
         <img className="inline h-8 align-middle mr-1" alt="Large box" src="img/banner/large_box.png" />
         <img className="inline h-3 align-bottom mr-6" alt="Small box" src="img/banner/small_box.png" />
@@ -10,6 +10,7 @@ function Banner(): JSX.Element {
         <a
           href="https://github.com/podman-desktop/community/issues/3"
           target="_blank"
+          rel="noreferrer"
           className="underline text-white ml-1">
           first Podman Desktop Community Meeting
         </a>{' '}
