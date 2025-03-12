51078999bbe331131391f731fc0aae6274b31264
Florent Benoit <fbenoit@redhat.com>
2025-03-12 19:39:26 +0100

    chore: adjust size Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- website/src/components/CommunityBanner.tsx
+++ website/src/components/CommunityBanner.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 
 function Banner(): JSX.Element {
   return (
-    <div className="w-full flex flex-row justify-center items-center py-2 bg-gradient-to-r from-sky-700 to-purple-700 overflow-hidden px-5">
+    <div className="w-full flex flex-row justify-center items-center py-1 bg-gradient-to-r from-sky-700 to-purple-700 overflow-hidden px-5">
       <div className="mx-3 relative text-white font-semibold">
-        <img className="inline h-10 align-middle mr-1" alt="Large box" src="img/banner/large_box.png" />
-        <img className="inline h-4 align-bottom mr-6" alt="Small box" src="img/banner/small_box.png" />
-        Join us for the first Podman Desktop Community Meeting on March 27th! RSVP
+        <img className="inline h-8 align-middle mr-1" alt="Large box" src="img/banner/large_box.png" />
+        <img className="inline h-3 align-bottom mr-6" alt="Small box" src="img/banner/small_box.png" />
+        Join us for the{' '}
         <a
           href="https://github.com/podman-desktop/community/issues/3"
           target="_blank"
           className="underline text-white ml-1">
-          here.
-        </a>
+          first Podman Desktop Community Meeting
+        </a>{' '}
+        on March 27th
         <img className="inline h-10 align-middle ml-6" alt="Large box" src="img/banner/large_box.png" />
       </div>
     </div>
