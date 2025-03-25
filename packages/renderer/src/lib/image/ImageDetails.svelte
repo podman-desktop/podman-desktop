0ae870056143d6c6b8ea823f4ce8985263577020
Sonia Sandler <ssandler@redhat.com>
2025-03-25 15:20:16 -0400

    chore: enable svelte/require-each-key rule - part 5 Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/renderer/src/lib/image/ImageDetails.svelte
+++ packages/renderer/src/lib/image/ImageDetails.svelte
@@ -136,7 +136,7 @@ onDestroy(() => {
     <svelte:fragment slot="subtitle">
       {#if image.badges.length}
         <div class="flex flex-row">
-          {#each image.badges as badge}
+          {#each image.badges as badge, index (`${badge.label}-${badge.color}-${index}`)}
             <Badge color={badge.color} label={badge.label} />
           {/each}
         </div>
