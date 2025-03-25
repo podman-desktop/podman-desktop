eeadc9b19c649085fef5d5f94413e51e02787a40
Sonia Sandler <ssandler@redhat.com>
2025-03-25 08:44:16 -0400

    chore: enable svelte/require-each-key rule - part 4 Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/renderer/src/lib/troubleshooting/TroubleshootingPageStoreDetails.svelte
+++ packages/renderer/src/lib/troubleshooting/TroubleshootingPageStoreDetails.svelte
@@ -58,7 +58,7 @@ async function fetch(): Promise<void> {
             {@const reverseOrderedBufferEvents = [...eventStoreInfo.bufferEvents].reverse()}
 
             <ul class="h-32 overflow-auto list-disc list-inside text-xs" aria-label="buffer-events">
-              {#each reverseOrderedBufferEvents as bufferEvent}
+              {#each reverseOrderedBufferEvents as bufferEvent (bufferEvent)}
                 <li aria-label={bufferEvent.name}>
                   {#if bufferEvent.skipped}
                     Skipped event
