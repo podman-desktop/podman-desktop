ee81d26508fa7c1e0e2a73e99530bec868b8d9a8
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-03-26 14:09:38 +0100

    fix(ui): make Carousel generic
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/ui/src/lib/carousel/CarouselTest.svelte
+++ packages/ui/src/lib/carousel/CarouselTest.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
 import Carousel from './Carousel.svelte';
+
+interface Item {
+  content: string;
+}
 </script>
 
 
-{#snippet card(cardItem: unknown)}
-<p>{cardItem}</p>
+{#snippet card(item: Item)}
+  <p>{item.content}</p>
 {/snippet}
 
 <div>
   <p class="text-lg first-letter:uppercase font-bold pb-5">Learning center:</p>
   <div class="bg-[var(--pd-content-card-bg)] p-4 rounded-lg">
-    <Carousel cards={['card 1', 'card 2', 'card 3']} {card} cardWidth={340} />
+    <Carousel cards={['card 1', 'card 2', 'card 3'].map((value) => ({ content: value }))} {card} cardWidth={340} />
   </div>
 </div>
