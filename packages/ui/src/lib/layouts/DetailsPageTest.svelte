6c16e711f79c9c9b5ee01e96b376ccb39cf2f08c
Philippe Martin <phmartin@redhat.com>
2025-03-27 18:40:53 +0100

    chore: add unit test Signed-off-by: Philippe Martin <phmartin@redhat.com>

---
+++ packages/ui/src/lib/layouts/DetailsPageTest.svelte
@@ -0,0 +1,18 @@
+<script>
+import DetailsPage from './DetailsPage.svelte';
+</script>
+<DetailsPage 
+  title="my-title"
+  titleDetail="my-details"
+  subtitle="my-subtitle"
+  breadcrumbLeftPart="bcLeft"
+  breadcrumbRightPart="bcRight"
+  breadcrumbTitle="bcTitle">
+
+  <svelte:fragment slot="content">My content slot</svelte:fragment>
+  <svelte:fragment slot="tabs">My tabs slot</svelte:fragment>
+  <svelte:fragment slot="icon">My icon slot</svelte:fragment>
+  <svelte:fragment slot="subtitle">My subtitle slot</svelte:fragment>
+  <svelte:fragment slot="actions">My actions slot</svelte:fragment>
+  <svelte:fragment slot="detail">My detail slot</svelte:fragment>
+</DetailsPage>
