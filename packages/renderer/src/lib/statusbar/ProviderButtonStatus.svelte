a272b8b79ae635d4281b5398b2a5b3e36d322bb0
Sonia Sandler <ssandler@redhat.com>
2025-03-31 13:04:08 -0400

    chore: add initial design Signed-off-by: Sonia Sandler <ssandler@redhat.com>

---
+++ packages/renderer/src/lib/statusbar/ProviderButtonStatus.svelte
@@ -0,0 +1,19 @@
+
+<script lang="ts">
+interface Props {
+  status?: string;
+  class?: string;
+}
+
+let { status, class: className = '' }: Props = $props();
+</script>
+
+{#if status === 'starting' || status === 'stopping'}
+  <div aria-label="Connection Status Icon" class="max-h-1 rounded-full animate-spin border border-solid border-[var(--pd-action-button-spinner)] border-t-transparent {className}"></div>
+{:else if status === 'ready' || status === 'started'}
+  <div aria-label="Connection Status Icon" class="h-[6px] w-[6px] absolute top-[0px] right-[-2px] rounded-full bg-[var(--pd-default-text)] border-1 border-[var(--pd-statusbar-bg)] {className}"></div>
+{:else if status === 'stopped'}
+  <div aria-label="Connection Status Icon" class="h-[4px] w-[20px] rotate-45 absolute top-[3px] left-[0px] rounded-full bg-[var(--pd-status-stopped)] border-1 border-[var(--pd-statusbar-bg)] {className}"></div>
+<!-- {:else if status === 'Update available'}
+  <div aria-label="Connection Status Icon" class="fa-solid fa-circle-up fa-xs absolute top-[0px] right-[-1px] rounded-full border-2 border-[var(--pd-statusbar-bg)] bg-[var(--pd-statusbar-bg)] {className}"></div> -->
+{/if}
