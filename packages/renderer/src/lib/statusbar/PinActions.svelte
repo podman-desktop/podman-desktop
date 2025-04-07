65516cb5e3f46fedbcb856ce16986070f6dad718
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-04-07 11:34:27 +0200

    fix(linter): adding missing key for each-block
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/statusbar/PinActions.svelte
+++ packages/renderer/src/lib/statusbar/PinActions.svelte
@@ -56,7 +56,7 @@ function pin(providerId: string): void {
 <div bind:this={outsideWindow}>
   {#if showMenu}
     <PinMenu>
-      {#each providers.entries() as [provider, pinned] }
+      {#each providers.entries() as [provider, pinned] (provider.id)}
         <ProviderButton
           class="w-full text-[var(--pd-dropdown-item-text)] hover:rounded-md hover:!bg-[var(--pd-dropdown-item-hover-bg)] hover:text-[var(--pd-dropdown-item-hover-text)]"
           provider={provider}
