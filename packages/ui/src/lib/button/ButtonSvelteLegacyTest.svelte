aa4a70b9ccc39c2042ee60acff3afb344e1ff486
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-04-08 11:26:59 +0200

    test(ui): adding svelte legacy usage for button
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

---
+++ packages/ui/src/lib/button/ButtonSvelteLegacyTest.svelte
@@ -0,0 +1,12 @@
+<!-- force the component to be legacy mode -->
+<svelte:options runes={false} />
+<script lang="ts">
+import { Button } from '/@/lib';
+
+export let onclick: () => void;
+export let content: string | undefined;
+</script>
+
+<Button on:click={onclick}>
+  {content}
+</Button>
