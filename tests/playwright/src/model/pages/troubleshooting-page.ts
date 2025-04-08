ebb624a0dc49d9e66efae1c02948719a70652c6e
Ondrej Dockal <odockal@redhat.com>
2025-04-08 17:50:09 +0200

    chore(test): update pom for troubleshooting
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- tests/playwright/src/model/pages/troubleshooting-page.ts
+++ tests/playwright/src/model/pages/troubleshooting-page.ts
@@ -26,6 +26,14 @@ export class TroubleshootingPage extends BasePage {
   readonly header: Locator;
   readonly tabs: Locator;
   readonly tabContent: Locator;
+  readonly containerConnectionsStatus: Locator;
+  readonly storesStatus: Locator;
+  readonly logsText: Locator;
+  readonly logsList: Locator;
+  readonly storesList: Locator;
+  readonly gatherLogsText: Locator;
+  readonly reconnectProvidersButton: Locator;
+  readonly reconnectProvidersStatus: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -33,26 +41,35 @@ export class TroubleshootingPage extends BasePage {
     this.heading = this.header.getByRole('heading', { name: 'Troubleshooting' });
     this.tabs = this.page.getByRole('region', { name: 'Tabs' });
     this.tabContent = this.page.getByRole('region', { name: 'Tab Content' });
+    // only available when specific tab is opened
+    this.containerConnectionsStatus = this.tabContent.getByRole('status', { name: 'Container Connections' });
+    this.storesStatus = this.tabContent.getByRole('status', { name: 'stores' });
+    this.logsText = this.tabContent.getByText('Logs', { exact: true });
+    this.gatherLogsText = this.tabContent.getByText('Gather Log Files');
+    this.logsList = this.tabContent.getByRole('list', { name: 'logs', exact: true });
+    this.storesList = this.tabContent.getByRole('list', { name: 'stores' });
+    this.reconnectProvidersButton = this.tabContent.getByRole('button', { name: 'Reconnect providers' });
+    this.reconnectProvidersStatus = this.tabContent.getByRole('status', { name: 'Reconnect Providers' });
   }
 
   public async openRepairConnections(): Promise<void> {
     await this.openTab('Repair & Connections');
-    await playExpect(this.tabContent.getByRole('status', { name: 'Container Connections' })).toBeVisible();
+    await playExpect(this.containerConnectionsStatus).toBeVisible();
   }
 
   public async openStores(): Promise<void> {
     await this.openTab('Stores');
-    await playExpect(this.tabContent.getByRole('status', { name: 'stores' })).toBeVisible();
+    await playExpect(this.storesStatus).toBeVisible();
   }
 
   public async openLogs(): Promise<void> {
     await this.openTab('Logs');
-    await playExpect(this.tabContent.getByText('Logs', { exact: true })).toBeVisible();
+    await playExpect(this.logsText).toBeVisible();
   }
 
   public async openGatherLogs(): Promise<void> {
     await this.openTab('Gather Logs');
-    await playExpect(this.tabContent.getByText('Gather Log Files')).toBeVisible();
+    await playExpect(this.gatherLogsText).toBeVisible();
   }
 
   private async openTab(tabName: string): Promise<void> {
@@ -64,14 +81,13 @@ export class TroubleshootingPage extends BasePage {
   // return locator for better processing in playwright assertions
   public async getLogs(): Promise<Locator> {
     await this.openLogs();
-    const list = this.tabContent.getByRole('list', { name: 'logs', exact: true });
-    return list;
+    await playExpect(this.logsList).toBeVisible();
+    return this.logsList;
   }
 
   public async refreshStore(storeName: string): Promise<void> {
     await this.openStores();
-    const stores = this.tabContent.getByRole('list', { name: 'stores' });
-    const store = stores.getByRole('listitem', { name: storeName });
+    const store = this.storesList.getByRole('listitem', { name: storeName });
     await playExpect(store).toBeVisible();
     await store.scrollIntoViewIfNeeded();
     const refreshButton = store.getByRole('button', { name: 'Refresh' });
@@ -81,20 +97,15 @@ export class TroubleshootingPage extends BasePage {
 
   public async getContainerConnectionsStatus(): Promise<string> {
     await this.openRepairConnections();
-    const connectProviderRegion = this.tabContent.getByRole('region', { name: 'Container Connections' });
-    const status = connectProviderRegion.getByRole('status', { name: 'Container Connections' });
-    await playExpect(status).toBeVisible();
-    return await status.innerText();
+    await playExpect(this.containerConnectionsStatus).toBeVisible();
+    return await this.containerConnectionsStatus.innerText();
   }
 
   public async reconnectProviders(): Promise<string> {
     await this.openRepairConnections();
-    const containerConnectionsRegion = this.tabContent.getByRole('region', { name: 'Container Connections' });
-    const reconnectButton = containerConnectionsRegion.getByRole('button', { name: 'Reconnect providers' });
-    await playExpect(reconnectButton).toBeEnabled();
-    await reconnectButton.click();
-    const status = containerConnectionsRegion.getByRole('status', { name: 'Reconnect Providers' });
-    await playExpect(status).toBeVisible();
-    return await status.innerText();
+    await playExpect(this.reconnectProvidersButton).toBeEnabled();
+    await this.reconnectProvidersButton.click();
+    await playExpect(this.reconnectProvidersStatus).toBeVisible();
+    return await this.reconnectProvidersStatus.innerText();
   }
 }
