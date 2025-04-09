05a8755d298b1c3a6afa64607fc62205acf52f41
Sonia Sandler <ssandler@redhat.com>
2025-04-09 12:36:13 -0400

    chore: update size Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/renderer/src/lib/statusbar/ProviderButtonStatus.svelte
+++ packages/renderer/src/lib/statusbar/ProviderButtonStatus.svelte
@@ -15,7 +15,7 @@ let { status, class: className = '' }: Props = $props();
 {#if status === 'starting' || status === 'stopping'}
   <div aria-label="Connection Status Icon" class="h-3 w-3 rounded-full animate-spin border border-solid border-[var(--pd-action-button-spinner)] border-t-transparent {className}"></div>
 {:else if status === 'ready' || status === 'started'}
-  <div aria-label="Connection Status Icon" class="h-[6px] w-[6px] absolute top-[0px] right-[-2px] rounded-full bg-[var(--pd-status-contrast)] border-1 border-[var(--pd-statusbar-bg)] {className}"></div>
+  <div aria-label="Connection Status Icon" class="h-[7px] w-[7px] absolute top-[0px] right-[-2px] rounded-full bg-[var(--pd-status-contrast)] border-1 border-[var(--pd-statusbar-bg)] {className}"></div>
 {:else if status === 'Update available'}
-  <IconImage image={arrowUp} aria-label="Connection Status Icon" class="h-[9px] w-auto absolute top-0 right-[-30%] {className}" />
+  <IconImage image={arrowUp} aria-label="Connection Status Icon" class="h-[12px] w-auto absolute top-0 right-[-30%] {className}" />
 {/if}
