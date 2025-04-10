dc4e82cf2d0b627baccf0a21d13fab859f6c0cc2
Jeff MAURY <jmaury@redhat.com>
2025-04-10 15:29:37 +0200

    fix: single context on MacOS/Linux and monitor connection state
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- extensions/docker/src/docker-context-synchronizer.ts
+++ extensions/docker/src/docker-context-synchronizer.ts
@@ -21,7 +21,7 @@ import { env, provider } from '@podman-desktop/api';
 import type { DockerContextHandler } from './docker-context-handler';
 
 export function toDockerContextName(name: string): string {
-  return name.startsWith('podman-') ? name : `podman-${name}`;
+  return env.isWindows ? (name.startsWith('podman-') ? name : `podman-${name}`) : 'podman';
 }
 
 export function toEndpoint(socketPath: string): string {
@@ -30,51 +30,36 @@ export function toEndpoint(socketPath: string): string {
 
 export class DockerContextSynchronizer implements Disposable {
   #disposable: Disposable[] = [];
-  #podmanConnections: string[] = [];
 
   constructor(private dockerContextHandler: DockerContextHandler) {
     this.#disposable.push(
-      provider.onDidRegisterContainerConnection(event => this.processAddedConnection(event.connection)),
-    );
-    this.#disposable.push(
-      provider.onDidUnregisterContainerConnection(event => this.processRemovedConnection(event.providerId)),
+      provider.onDidUpdateContainerConnection(event => this.processUpdatedConnection(event.connection)),
     );
   }
 
   async init(): Promise<void> {
-    const podmanConnections = provider
+    const connections = provider
       .getContainerConnections()
-      .filter(connection => connection.connection.type === 'podman');
-    for (const connection of podmanConnections) {
-      await this.processAddedConnection(connection.connection);
+      .filter(connection => connection.connection.status() === 'started');
+    for (const connection of connections) {
+      await this.processUpdatedConnection(connection.connection);
     }
   }
 
-  private async processAddedConnection(connection: ContainerProviderConnection): Promise<void> {
+  protected async processUpdatedConnection(connection: ContainerProviderConnection): Promise<void> {
     if (connection.type === 'podman') {
-      try {
-        await this.dockerContextHandler.createContext({
-          name: toDockerContextName(connection.name),
-          metadata: { description: `Podman machine ${connection.name}` },
-          endpoints: { docker: { host: toEndpoint(connection.endpoint.socketPath) } },
-        });
-        this.#podmanConnections.push(connection.name);
-      } catch (error: unknown) {
-        console.warn(`Error creating Docker context for Podman machine ${connection.name}`, error);
-      }
-    }
-  }
-
-  protected async processRemovedConnection(providerId: string): Promise<void> {
-    if (providerId === 'podman') {
-      const podmanConnections = provider
-        .getContainerConnections()
-        .filter(connection => connection.providerId === 'podman')
-        .map(connection => connection.connection.name);
-      const podmanConnectionsSet = new Set(podmanConnections);
-      const removedConnections = this.#podmanConnections.filter(name => !podmanConnectionsSet.has(name));
-      for (const name of removedConnections) {
-        const dockerContextName = toDockerContextName(name);
+      if (connection.status() === 'started') {
+        try {
+          await this.dockerContextHandler.createContext({
+            name: toDockerContextName(connection.name),
+            metadata: { description: `Podman machine ${connection.name}` },
+            endpoints: { docker: { host: toEndpoint(connection.endpoint.socketPath) } },
+          });
+        } catch (error: unknown) {
+          console.warn(`Error creating Docker context for Podman machine ${connection.name}`, error);
+        }
+      } else if (connection.status() === 'stopped') {
+        const dockerContextName = toDockerContextName(connection.name);
         try {
           await this.dockerContextHandler.removeContext(dockerContextName);
         } catch (error: unknown) {
