f00fbdab7d575d1f50d985ae7ed7a02934678c03
Sonia Sandler <ssandler@redhat.com>
2025-05-02 17:31:19 -0400

    chore: update expandable button component Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- website/src/components/ExpandableFAQ.tsx
+++ website/src/components/ExpandableFAQ.tsx
@@ -10,14 +10,12 @@ export const ExpandableFAQ = (props: ExpandableFAQProps): JSX.Element => {
 
   return (
     <div>
-      <div className="flex flex-row items-center justify-start gap-6 p-2.5">
+      <button onClick={() => setExpanded(!expanded)} className="flex flex-row items-center justify-start gap-6 p-2.5">
         <div className={expanded ? 'transition rotate-90 duration-500' : 'transition rotate-0 duration-500'}>
-          <button onClick={() => setExpanded(!expanded)} className="text-[32px] leading-[1.7] font-bold">
-            +
-          </button>
+          <div className="text-[32px] leading-[1.7] font-bold">+</div>
         </div>
         <span className="text-xl font-semibold leading-1.8">{props.title}</span>
-      </div>
+      </button>
       <div className="w-full h-1 bg-white"></div>
       {expanded && <p className="text-base leading-[1.8] mx-13.5 my-6 font-normal tracking-[0.2px]">{props.text}</p>}
     </div>
