602cd703d5cb6ac9099f808b791a9791271746df
Sonia Sandler <ssandler@redhat.com>
2025-05-14 10:33:49 -0400

    chore: make the image path input field clearable Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/ui/src/lib/inputs/Input.svelte
+++ packages/ui/src/lib/inputs/Input.svelte
@@ -79,8 +79,9 @@ async function onClear(): Promise<void> {
     {#if clearable}
       <button
         class="px-0.5 cursor-pointer text-[color:var(--pd-input-field-icon)] group-hover:text-[color:var(--pd-input-field-hover-icon)] group-focus-within:text-[color:var(--pd-input-field-focused-icon)]"
-        class:hidden={!value || readonly || disabled}
+        class:hidden={!value || disabled}
         aria-label="clear"
+        type="button"
         on:click={onClear}>
         <Fa icon={faXmark} />
       </button>
