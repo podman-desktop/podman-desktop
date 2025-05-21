c4065c4d917db84aa03cb6178b2afeb8acb3df79
Philippe Martin <phmartin@redhat.com>
2025-05-21 15:11:58 +0200

    feat: make clear container logs permanent Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/renderer/src/lib/container/ContainerDetailsLogs.svelte
+++ packages/renderer/src/lib/container/ContainerDetailsLogs.svelte
@@ -4,6 +4,9 @@ import '@xterm/xterm/css/xterm.css';
 import { EmptyScreen } from '@podman-desktop/ui-svelte';
 import type { Terminal } from '@xterm/xterm';
 import { mount, onDestroy, onMount } from 'svelte';
+import type { Unsubscriber } from 'svelte/store';
+
+import { containerLogsCleared, getKeyValue } from '/@/stores/container-logs-cleared';
 
 import { isMultiplexedLog } from '../stream/stream-utils';
 import NoLogIcon from '../ui/NoLogIcon.svelte';
@@ -24,7 +27,7 @@ $: {
     refContainer &&
     (refContainer.id !== container.id || (refContainer.state !== container.state && container.state !== 'EXITED'))
   ) {
-    logsTerminal?.clear();
+    clearTerminal();
     fetchContainerLogs().catch((err: unknown) => console.error(`Error fetching container logs ${container.id}`, err));
   }
   refContainer = container;
@@ -33,17 +36,32 @@ let terminalParentDiv: HTMLDivElement;
 
 let logsTerminal: Terminal;
 
+let logsLength = 0;
+let skip = 0;
+let cancellableTokenId: number | undefined = undefined;
+let containerLogsClearedUnsubscriber: Unsubscriber | undefined = undefined;
+
+let containerDetailsLogsClearSetRevert: ((revert: boolean) => void) | undefined = undefined;
+
+$: {
+  if (skip > 0 && logsLength === skip) {
+    containerDetailsLogsClearSetRevert?.(true);
+  } else {
+    containerDetailsLogsClearSetRevert?.(false);
+  }
+}
+
 function callback(name: string, data: string): void {
   if (name === 'first-message') {
     noLogs = false;
     // clear on the first message
-    logsTerminal?.clear();
+    clearTerminal();
   } else if (name === 'data') {
     noLogs = false;
     if (isMultiplexedLog(data)) {
-      logsTerminal?.write(data.substring(8) + '\r');
+      addDataToTerminal(data.substring(8));
     } else {
-      logsTerminal?.write(data + '\r');
+      addDataToTerminal(data);
     }
   }
   if (!noLogs) {
@@ -52,8 +70,17 @@ function callback(name: string, data: string): void {
 }
 
 async function fetchContainerLogs(): Promise<void> {
+  containerLogsClearedUnsubscriber?.();
+  containerLogsClearedUnsubscriber = containerLogsCleared.subscribe(cleared => {
+    skip = cleared.get(getKeyValue(container.id, container.engineId)) ?? 0;
+  });
+
+  if (cancellableTokenId) {
+    await window.cancelToken(cancellableTokenId);
+  }
+  cancellableTokenId = await window.getCancellableTokenSource();
   // grab logs of the container
-  await window.logsContainer({ engineId: container.engineId, containerId: container.id, callback });
+  await window.logsContainer({ engineId: container.engineId, containerId: container.id, callback, cancellableTokenId });
 }
 
 function afterTerminalInit(): void {
@@ -61,20 +88,55 @@ function afterTerminalInit(): void {
   let xtermElement = terminalParentDiv.querySelector('.xterm');
   xtermElement ??= terminalParentDiv;
   // add svelte component using this xterm element
-  mount(ContainerDetailsLogsClear, {
+  const { setRevert } = mount(ContainerDetailsLogsClear, {
     target: xtermElement,
     props: {
       terminal: logsTerminal,
+      onclear: async () => {
+        if (logsLength === skip) {
+          setSkip(0);
+        } else {
+          setSkip(logsLength);
+        }
+        await fetchContainerLogs();
+      },
     },
   });
+  containerDetailsLogsClearSetRevert = setRevert;
+}
+
+function setSkip(n: number): void {
+  const storeValue = $containerLogsCleared;
+  if (n) {
+    storeValue.set(getKeyValue(container.id, container.engineId), n);
+  } else {
+    storeValue.delete(getKeyValue(container.id, container.engineId));
+  }
+  containerLogsCleared.set(storeValue);
+}
+
+function clearTerminal(): void {
+  logsTerminal?.clear();
+  logsLength = 0;
+}
+
+function addDataToTerminal(data: string): void {
+  if (logsLength >= skip) {
+    logsTerminal?.write(data + '\r');
+  }
+  logsLength += data.length + 1;
 }
 
 onMount(async () => {
   await fetchContainerLogs();
 });
 
-onDestroy(() => {
+onDestroy(async () => {
   logsTerminal?.dispose();
+  containerLogsClearedUnsubscriber?.();
+  if (cancellableTokenId) {
+    await window.cancelToken(cancellableTokenId);
+  }
 });
 </script>
 
