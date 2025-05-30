82ee7873fdcaa63ca78dc6ed4448f42d1f80c23d
Evzen Gasta <evzen.ml@seznam.cz>
2025-05-30 08:18:52 +0200

    feat(docs): added ideal image plugin
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>

--- website/src/components/TestimonialCard.tsx
+++ website/src/components/TestimonialCard.tsx
@@ -1,5 +1,6 @@
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Image from '@theme/IdealImage';
 import React from 'react';
 
 export type TestimonialCardProps = {
@@ -20,13 +21,13 @@ export const TestimonialCard = (props: TestimonialCardProps): JSX.Element => {
 
   return (
     <div className="relative min-w-fit w-full h-full bg-gradient-to-br from-sky-500 to-fuschia-500 p-[2px] rounded-lg ">
-      <img src="/img/quote.svg" alt="Quote symbol" className="absolute right-1/15 -top-6.5 rounded-full w-13 h-13 " />
+      <Image img="/img/quote.svg" alt="Quote symbol" className="absolute right-1/15 -top-6.5 rounded-full w-13 h-13 " />
       <div className="dark:bg-fuschia-900 bg-purple-200 rounded-lg h-full">
         <div className="rounded-lg p-6 h-full bg-white/50 dark:bg-black/70">
           <div className="flex flex-row items-center">
             {props.userImage && !showIcon ? (
-              <img
-                src={props.userImage}
+              <Image
+                img={props.userImage}
                 onError={() => setShowIcon(true)}
                 alt="user profile image"
                 className="w-13 h-13 rounded-full"
