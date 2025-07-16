6c63c745e20b289458d9044563b9017a66a36d28
Simon Rey <sfbrey+eqqe@gmail.com>
2025-07-16 10:23:03 +0200

    refactor(website): remove intermediate const
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>

--- website/src/plugins/github-service.ts
+++ website/src/plugins/github-service.ts
@@ -28,7 +28,6 @@ export class GitHubService {
         );
       }
 
-      // Helper function to find an asset or throw an error
       const findAssetOrThrow = (predicate: (asset: (typeof assets)[0]) => boolean, assetName: string): string => {
         const asset = assets.find(predicate);
         if (!asset) {
@@ -36,13 +35,6 @@ export class GitHubService {
         }
         return asset.browser_download_url;
       };
-
-      const flatpakUrl = findAssetOrThrow(a => a.name.endsWith('.flatpak'), 'Linux Flatpak');
-      const linuxArmUrl = findAssetOrThrow(a => a.name.endsWith('-arm64.tar.gz'), 'Linux ARM64 .tar.gz');
-      const linuxAmdUrl = findAssetOrThrow(
-        a => a.name.endsWith('.tar.gz') && !a.name.includes('arm64'),
-        'Linux AMD64 .tar.gz',
-      );
 
       const macosArm64Url = findAssetOrThrow(
         a => a.name.endsWith('-arm64.dmg') && !a.name.includes('airgap'),
@@ -52,70 +44,63 @@ export class GitHubService {
         a => a.name.endsWith('-x64.dmg') && !a.name.includes('airgap'),
         'macOS x64 DMG',
       );
-      const macosUniversalUrl = findAssetOrThrow(
-        a =>
-          a.name.endsWith('.dmg') &&
-          !a.name.includes('airgap') &&
-          a.name !== macosArm64Url.split('/').pop() &&
-          a.name !== macosX64Url.split('/').pop(),
-        'macOS Universal DMG',
-      );
-      const macosAirgapX64Url = findAssetOrThrow(
-        a => a.name.endsWith('-x64.dmg') && a.name.includes('airgap'),
-        'macOS Airgap x64 DMG',
-      );
-      const macosAirgapArm64Url = findAssetOrThrow(
-        a => a.name.endsWith('-arm64.dmg') && a.name.includes('airgap'),
-        'macOS Airgap ARM64 DMG',
-      );
-
-      const windowsSetupX64Url = findAssetOrThrow(
-        a => a.name.endsWith('-setup-x64.exe') && !a.name.includes('airgap'),
-        'Windows Setup x64 EXE',
-      );
-      const windowsSetupArm64Url = findAssetOrThrow(
-        a => a.name.endsWith('-setup-arm64.exe') && !a.name.includes('airgap'),
-        'Windows Setup ARM64 EXE',
-      );
-      const windowsPortableX64Url = findAssetOrThrow(
-        a => a.name.endsWith('-x64.exe') && !a.name.includes('setup') && !a.name.includes('airgap'),
-        'Windows Portable x64 EXE',
-      );
-      const windowsPortableArm64Url = findAssetOrThrow(
-        a => a.name.endsWith('-arm64.exe') && !a.name.includes('setup') && !a.name.includes('airgap'),
-        'Windows Portable ARM64 EXE',
-      );
-      const windowsAirgapSetupX64Url = findAssetOrThrow(
-        a => a.name.endsWith('-setup-x64.exe') && a.name.includes('airgap'),
-        'Windows Airgap Setup x64 EXE',
-      );
-      const windowsAirgapSetupArm64Url = findAssetOrThrow(
-        a => a.name.endsWith('-setup-arm64.exe') && a.name.includes('airgap'),
-        'Windows Airgap Setup ARM64 EXE',
-      );
 
       return {
         latestRelease: {
           version: tag_name.replace(/^v/, ''),
           linux: {
-            flatpak: flatpakUrl,
-            arm64: linuxArmUrl,
-            amd64: linuxAmdUrl,
+            flatpak: findAssetOrThrow(a => a.name.endsWith('.flatpak'), 'Linux Flatpak'),
+            arm64: findAssetOrThrow(a => a.name.endsWith('-arm64.tar.gz'), 'Linux ARM64 .tar.gz'),
+            amd64: findAssetOrThrow(
+              a => a.name.endsWith('.tar.gz') && !a.name.includes('arm64'),
+              'Linux AMD64 .tar.gz',
+            ),
           },
           macos: {
-            universal: macosUniversalUrl,
+            universal: findAssetOrThrow(
+              a =>
+                a.name.endsWith('.dmg') &&
+                !a.name.includes('airgap') &&
+                a.name !== macosArm64Url.split('/').pop() &&
+                a.name !== macosX64Url.split('/').pop(),
+              'macOS Universal DMG',
+            ),
             x64: macosX64Url,
             arm64: macosArm64Url,
-            airgapsetupX64: macosAirgapX64Url,
-            airgapsetupArm64: macosAirgapArm64Url,
+            airgapsetupX64: findAssetOrThrow(
+              a => a.name.endsWith('-x64.dmg') && a.name.includes('airgap'),
+              'macOS Airgap x64 DMG',
+            ),
+            airgapsetupArm64: findAssetOrThrow(
+              a => a.name.endsWith('-arm64.dmg') && a.name.includes('airgap'),
+              'macOS Airgap ARM64 DMG',
+            ),
           },
           windows: {
-            setupX64: windowsSetupX64Url,
-            setupArm64: windowsSetupArm64Url,
-            binaryX64: windowsPortableX64Url,
-            binaryArm64: windowsPortableArm64Url,
-            airgapsetupX64: windowsAirgapSetupX64Url,
-            airgapsetupArm64: windowsAirgapSetupArm64Url,
+            setupX64: findAssetOrThrow(
+              a => a.name.endsWith('-setup-x64.exe') && !a.name.includes('airgap'),
+              'Windows Setup x64 EXE',
+            ),
+            setupArm64: findAssetOrThrow(
+              a => a.name.endsWith('-setup-arm64.exe') && !a.name.includes('airgap'),
+              'Windows Setup ARM64 EXE',
+            ),
+            binaryX64: findAssetOrThrow(
+              a => a.name.endsWith('-x64.exe') && !a.name.includes('setup') && !a.name.includes('airgap'),
+              'Windows Portable x64 EXE',
+            ),
+            binaryArm64: findAssetOrThrow(
+              a => a.name.endsWith('-arm64.exe') && !a.name.includes('setup') && !a.name.includes('airgap'),
+              'Windows Portable ARM64 EXE',
+            ),
+            airgapsetupX64: findAssetOrThrow(
+              a => a.name.endsWith('-setup-x64.exe') && a.name.includes('airgap'),
+              'Windows Airgap Setup x64 EXE',
+            ),
+            airgapsetupArm64: findAssetOrThrow(
+              a => a.name.endsWith('-setup-arm64.exe') && a.name.includes('airgap'),
+              'Windows Airgap Setup ARM64 EXE',
+            ),
           },
         },
       };
