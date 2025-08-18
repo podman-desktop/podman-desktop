2cedef7c2d250bf661c510ee248a4e125339c6f6
Sonia Sandler <ssandler@redhat.com>
2025-08-18 08:46:55 -0400

    chore: update comment based on reviews Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- extensions/podman/packages/extension/src/utils/registry-setup.ts
+++ extensions/podman/packages/extension/src/utils/registry-setup.ts
@@ -126,7 +126,7 @@ export class RegistrySetup {
           // split the decoded string into username and password separated by :
           const [username, secret] = decoded.split(':');
 
-          // do not encode the registry again if the values are not different from the ones in the file
+          // only encode if values have changed from what's stored in the auth file
           encode = !(username === registry.username && secret === registry.secret);
         }
 
