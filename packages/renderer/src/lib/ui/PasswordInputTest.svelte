94b8b58394e06c9b7ba199af9af1eb276fdb3d91
Florent Benoit <fbenoit@redhat.com>
2025-09-04 11:28:01 +0200

    fix: avoid propagating event of PasswordInput component for show/hide
    fixes https://github.com/podman-desktop/podman-desktop/issues/13798
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ packages/renderer/src/lib/ui/PasswordInputTest.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+import PasswordInput from './PasswordInput.svelte';
+
+export let onSubmit: (event: Event) => void;
+</script>
+
+<form on:submit={onSubmit}>
+  <PasswordInput id="foo" password="secret" readonly={false} />
+  <button type="submit">Submit</button>
+</form>
