1bba5da899012f72f3879853360033725d041637
Sonia Sandler <ssandler@redhat.com>
2025-09-11 10:58:57 -0400

    chore: update images links and apply reviews Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- website/src/components/3MBanner.tsx
+++ website/src/components/3MBanner.tsx
@@ -7,41 +7,41 @@ function Banner(): JSX.Element {
         <img
           className="absolute sm:left-0 sm:top-[-10px] sm:w-[76px] left-0 top-[-7px] w-[60px]"
           alt="3 balloon"
-          src="img/3MBanner/balloon-3.png"
+          src="https://github.com/podman-desktop/3m/blob/ec786501bbca0591c3982146ed919f27c25ca806/img/balloon-3.png?raw=true"
         />
         <img
           className="absolute sm:left-[55px] sm:top-[9px] sm:w-[64px] top-[6px] left-[40px] w-[50px] -rotate-10 "
           alt="0 balloon"
-          src="img/3MBanner/balloon-0.png"
+          src="https://github.com/podman-desktop/3m/blob/ec786501bbca0591c3982146ed919f27c25ca806/img/balloon-0.png?raw=true"
         />
         <img
           className="absolute sm:left-[87px] sm:top-[19px] sm:w-[64px] top-[14px] left-[62px] w-[50px] "
           alt="0 balloon"
-          src="img/3MBanner/balloon-0.png"
+          src="https://github.com/podman-desktop/3m/blob/ec786501bbca0591c3982146ed919f27c25ca806/img/balloon-0.png?raw=true"
         />
         <img
           className="absolute sm:left-[121px] sm:top-[8px] sm:w-[64px] top-[5px] left-[86px] w-[50px] "
           alt="0 balloon"
-          src="img/3MBanner/balloon-0.png"
+          src="https://github.com/podman-desktop/3m/blob/ec786501bbca0591c3982146ed919f27c25ca806/img/balloon-0.png?raw=true"
         />
         <img
           className="absolute sm:left-[173px] sm:top-[16px] sm:w-[64px] top-[12px] left-[123px] w-[50px] -rotate-7 "
           alt="0 balloon"
-          src="img/3MBanner/balloon-0.png"
+          src="https://github.com/podman-desktop/3m/blob/ec786501bbca0591c3982146ed919f27c25ca806/img/balloon-0.png?raw=true"
         />
         <img
           className="absolute sm:left-[209px] sm:top-[12px] sm:w-[64px] top-[9px] left-[144px] w-[50px] rotate-6 "
           alt="0 balloon"
-          src="img/3MBanner/balloon-0.png"
+          src="https://github.com/podman-desktop/3m/blob/ec786501bbca0591c3982146ed919f27c25ca806/img/balloon-0.png?raw=true"
         />
         <img
           className="absolute sm:left-[245px] sm:top-[4px] sm:w-[64px] top-[3px] left-[170px] w-[50px] "
           alt="0 balloon"
-          src="img/3MBanner/balloon-0.png"
+          src="https://github.com/podman-desktop/3m/blob/ec786501bbca0591c3982146ed919f27c25ca806/img/balloon-0.png?raw=true"
         />
       </div>
-      <div className="flex-initial whitespace-normal break-words sm:text-2xl text-2xl overflow-hidden font-black">
-        downloads. <span className="font-light sm:text-2xl text-base"> Proven in the real world.</span>{' '}
+      <div className="flex-initial whitespace-normal break-words sm:text-2xl text-xl overflow-hidden font-black">
+        DOWNLOADS. <span className="font-light sm:text-2xl text-base"> Proven in the real world.</span>{' '}
         <a href="https://3m.podman-desktop.io" className="font-semibold sm:text-2xl text-base underline text-red-200">
           Celebrate!
         </a>
