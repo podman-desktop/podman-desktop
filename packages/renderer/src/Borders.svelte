69624c429ba0f870cf32ae983b42d15662807c69
Sonia Sandler <ssandler@redhat.com>
2025-09-15 13:19:51 -0400

    chore: update border imports Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/renderer/src/Borders.svelte
+++ packages/renderer/src/Borders.svelte
@@ -1,20 +1,29 @@
 <script lang="ts">
-import { images } from './border-assets/linux/images';
+let theme: 'light' | 'dark' = 'light'; // will also have light theme
+let os = 'linux';
 
-let theme: 'light' | 'dark' = 'dark'; // will also have light theme
+const topLeftBorder = new URL(`./border-assets/${os}/${os}-${theme}-1.png`, import.meta.url).href;
+const topRightBorder = new URL(`./border-assets/${os}/${os}-${theme}-2.png`, import.meta.url).href;
+const bottomLeftBorder = new URL(`./border-assets/${os}/${os}-${theme}-3.png`, import.meta.url).href;
+const bottomRightBorder = new URL(`./border-assets/${os}/${os}-${theme}-4.png`, import.meta.url).href;
+
+const topBorder = new URL(`./border-assets/${os}/${os}-${theme}-t.png`, import.meta.url).href;
+const bottomBorder = new URL(`./border-assets/${os}/${os}-${theme}-b.png`, import.meta.url).href;
+const leftBorder = new URL(`./border-assets/${os}/${os}-${theme}-l.png`, import.meta.url).href;
+const rightBorder = new URL(`./border-assets/${os}/${os}-${theme}-r.png`, import.meta.url).href;
 </script>
 
 <div class="absolute top-0 left-0 w-screen h-screen grid grid-cols-[40px_1fr_40px] grid-rows-[40px_1fr_40px] z-0">
-  <img src={images[theme]['tl']} class="w-[40px] h-[40px]" alt='some alt'/>
-  <img src={images[theme]['t']} class="w-full h-[40px] object-fill" alt='some alt'/>
-  <img src={images[theme]['tr']} class="w-[40px] h-[40px]" alt='some alt'/>
+  <img src={topLeftBorder} class="w-[40px] h-[40px]" alt="Top left border"/>
+  <img src={topBorder} class="w-full h-[40px] object-fill" alt="Top border"/>
+  <img src={topRightBorder} class="w-[40px] h-[40px]" alt="Top right border"/>
 
-  <img src={images[theme]['l']} class="w-[40px] h-full object-fill"  alt='some alt'/>
+  <img src={leftBorder} class="w-[40px] h-full object-fill"  alt="Left border"/>
   <div class="bg-[var(--pd-content-bg)]"></div>
-  <img src={images[theme]['r']} class="w-[40px] h-full object-fill" alt='some alt'/>
+  <img src={rightBorder} class="w-[40px] h-full object-fill" alt="Right border"/>
 
-  <img src={images[theme]['bl']} class="w-[40px] h-[40px]" alt='some alt'/>
-  <img src={images[theme]['b']} class="w-full h-[40px] object-fill"  alt='some alt'/>
-  <img src={images[theme]['br']} class="w-[40px] h-[40px]" alt='some alt'/>
+  <img src={bottomLeftBorder} class="w-[40px] h-[40px]" alt="Bottom left border"/>
+  <img src={bottomBorder} class="w-full h-[40px] object-fill"  alt="Bottom border"/>
+  <img src={bottomRightBorder} class="w-[40px] h-[40px]" alt="Bottom right border"/>
 
 </div>
