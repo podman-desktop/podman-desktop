6670e2afd6d406ac00c3d5a1802dfc6908de9a05
Charlie Drage <charlie@charliedrage.com>
2025-09-24 15:43:28 -0400

    chore: load 'managed by' default-settings.json into configuration scope
    ### What does this PR do?
    
    Part of the epic: https://github.com/podman-desktop/podman-desktop/issues/13898 to support
    a managed configuration.
    
    This PR will load a "managed by" default settings from a pre-defined file on the user's OS. These are listed in the issue: https://github.com/podman-desktop/podman-desktop/issues/13894
    
    Mac: `/Library/Application\ Support/com.podman.desktop/default-settings.json`
    Windows: `%PROGRAMDATA%\PodmanDesktop\default-settings.json`
    Linux: `/usr/share/podman-desktop/default-settings.json`
    
    This will then load it into a separate getConfiguration scope.
    (MANAGED_DEFAULTS).
    
    ### Screenshot / video of UI
    
    <!-- If this PR is changing UI, please include
    screenshots or screencasts showing the difference -->
    
    N/A, this is internal code and no screenshots.
    
    ### What issues does this PR fix or reference?
    
    <!-- Include any related issues from Podman Desktop
    repository (or from another issue tracker). -->
    
    Closes https://github.com/podman-desktop/podman-desktop/issues/13894
    
    ### How to test this PR?
    
    1. Create a separate "managed" configuration file.
    
    Mac: `/Library/Application
    Support/com.podman.desktop/default-settings.json`
    
    By copying over your current one to see values change:
    
    ```
    sudo  cp ~/.local/share/containers/podman-desktop/configuration/settings.json /Library/Application\ Support/com.podman.desktop/default-settings.json
    ```
    
    2. Edit some "renderer" code to see if you can "get" the configuration
       through the scope. For example:
       `packages/renderer/src/lib/dashbvoard/DashboardPage.svelte`
    
    ```ts
    onMount(async () => {
      try {
        console.log('Configuration scopes:', {
          DEFAULT: CONFIGURATION_DEFAULT_SCOPE,
          MANAGED_DEFAULTS: CONFIGURATION_MANAGED_DEFAULTS_SCOPE
        });
    
        // Show how configuration values can be fetched from default as well as managed default scopes
        const defaultScopeValue = await window.getConfigurationValue<string>('welcome.version', CONFIGURATION_DEFAULT_SCOPE);
        const managedScopeValue = await window.getConfigurationValue<string>('welcome.version', CONFIGURATION_MANAGED_DEFAULTS_SCOPE);
        console.log('Default scope value:', defaultScopeValue);
        console.log('Admin default scope value:', managedScopeValue);
    
      } catch (error) {
        console.error('Error fetching configuration values:', error);
      }
    });
    ```
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/main/src/plugin/configuration-registry.spec.ts
+++ packages/main/src/plugin/configuration-registry.spec.ts
@@ -46,6 +46,11 @@ vi.mock('node:fs', () => ({
   },
 }));
 
+// mock DefaultConfiguration for the new managed defaults functionality
+vi.mock('./default-configuration.js', () => ({
+  DefaultConfiguration: vi.fn(),
+}));
+
 let configurationRegistry: ConfigurationRegistry;
 
 const getConfigurationDirectoryMock = vi.fn();
