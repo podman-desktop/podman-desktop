d05a905e94cc92f3701d8a84b325cc0949b6cf87
Vladimir Lazar <vlazar@redhat.com>
2025-09-25 12:28:30 +0200

    chore(test): add k8s dashboard validation e2e Signed-off-by: Vladimir Lazar <vlazar@redhat.com>

--- tests/playwright/src/model/pages/kubernetes-dashboard-page.ts
+++ tests/playwright/src/model/pages/kubernetes-dashboard-page.ts
@@ -19,6 +19,7 @@
 import type { Locator, Page } from '@playwright/test';
 import { expect as playExpect } from '@playwright/test';
 
+import { KubernetesResources } from '../core/types';
 import { MainPage } from './main-page';
 
 export class KubernetesDashboardPage extends MainPage {
@@ -26,11 +27,31 @@ export class KubernetesDashboardPage extends MainPage {
   readonly namespaceDropdownButton: Locator;
   readonly currentNamespace: Locator;
 
+  readonly nodesMetricsButton: Locator;
+  readonly podsMetricsButton: Locator;
+  readonly deploymentsMetricsButton: Locator;
+  readonly servicesMetricsButton: Locator;
+  readonly ingressesMetricsButton: Locator;
+  readonly pvcsMetricsButton: Locator;
+  readonly configMapsMetricsButton: Locator;
+  readonly jobsMetricsButton: Locator;
+  readonly cronJobsMetricsButton: Locator;
+
   constructor(page: Page) {
     super(page, 'Dashboard');
     this.namespaceLocator = this.page.getByLabel('Kubernetes Namespace', { exact: true });
     this.namespaceDropdownButton = this.namespaceLocator.getByRole('button', { name: 'Namespace' });
     this.currentNamespace = this.namespaceLocator.getByLabel('hidden input', { exact: true });
+
+    this.nodesMetricsButton = this.metricsLocatorByName(KubernetesResources.Nodes);
+    this.podsMetricsButton = this.metricsLocatorByName(KubernetesResources.Pods);
+    this.deploymentsMetricsButton = this.metricsLocatorByName(KubernetesResources.Deployments);
+    this.servicesMetricsButton = this.metricsLocatorByName(KubernetesResources.Services);
+    this.ingressesMetricsButton = this.metricsLocatorByName(KubernetesResources.IngeressesRoutes);
+    this.pvcsMetricsButton = this.metricsLocatorByName(KubernetesResources.PVCs);
+    this.configMapsMetricsButton = this.metricsLocatorByName(KubernetesResources.ConfigMapsSecrets);
+    this.jobsMetricsButton = this.metricsLocatorByName(KubernetesResources.Jobs);
+    this.cronJobsMetricsButton = this.metricsLocatorByName(KubernetesResources.Cronjobs);
   }
 
   async changeNamespace(name: string): Promise<void> {
@@ -48,7 +69,35 @@ export class KubernetesDashboardPage extends MainPage {
     await namespaceLocator.click();
   }
 
+  async getCurrentTotalCountForResource(name: KubernetesResources): Promise<number> {
+    const currentResource = this.metricsLocatorByName(name);
+    const totalCountLocator = currentResource.getByLabel(`${name} count`, { exact: true });
+
+    if ((await totalCountLocator.count()) === 0) {
+      throw new Error(`No total count locator found for resource: ${name}`);
+    }
+
+    const totalCountText = await totalCountLocator.textContent();
+    return totalCountText ? parseInt(totalCountText, 10) : 0;
+  }
+
+  async getCurrentActiveCountForResource(name: KubernetesResources): Promise<number> {
+    const currentResource = this.metricsLocatorByName(name);
+    const activeCountLocator = currentResource.getByLabel(`${name} active count`, { exact: true });
+
+    if ((await activeCountLocator.count()) === 0) {
+      throw new Error(`No active count locator found for resource: ${name}`);
+    }
+
+    const activeCountText = await activeCountLocator.textContent();
+    return activeCountText ? parseInt(activeCountText, 10) : 0;
+  }
+
   private namespaceLocatorByName(name: string): Locator {
     return this.namespaceLocator.getByRole('button', { name, exact: true });
+  }
+
+  private metricsLocatorByName(name: KubernetesResources): Locator {
+    return this.page.getByRole('button').filter({ hasText: name });
   }
 }
