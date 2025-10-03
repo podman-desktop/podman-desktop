492f0bc9ee1aa309069da3e938a5ba33c009c493
Marcel Bertagnini <mbertagn@redhat.com>
2025-10-03 13:43:32 +0200

    feat: added component for handling enums for booleans Signed-off-by: Marcel Bertagnini <mbertagn@redhat.com>

---
+++ packages/renderer/src/lib/ui/BooleanEnumDisplay.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+interface Props {
+  value: boolean;
+  options: string[];
+  description?: string;
+  id?: string;
+}
+
+const { value, options, description, id }: Props = $props();
+
+const displayText = $derived.by(() => {
+  if (options.length !== 2) {
+    return undefined;
+  }
+  return options[value ? 1 : 0];
+});
+</script>
+
+{#if displayText}
+ <span aria-label="{description ?? id}: {displayText}">
+  {displayText}
+ </span>
+{/if}
