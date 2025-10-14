de737b89be39d0ac538e7923515f00ef4144c8e9
Sonia Sandler <ssandler@redhat.com>
2025-10-13 22:35:59 -0400

    chore: add tests Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/renderer/src/App.svelte
+++ packages/renderer/src/App.svelte
@@ -56,6 +56,7 @@ import KubePodsList from './lib/kube/pods/PodsList.svelte';
 import PortForwardingList from './lib/kubernetes-port-forward/PortForwardingList.svelte';
 import ManifestDetails from './lib/manifest/ManifestDetails.svelte';
 import NetworksList from './lib/network/NetworksList.svelte';
+import NetworkDetails from './lib/network/NetworkDetails.svelte';
 import NodeDetails from './lib/node/NodeDetails.svelte';
 import NodesList from './lib/node/NodesList.svelte';
 import Onboarding from './lib/onboarding/Onboarding.svelte';
@@ -87,7 +88,6 @@ import Route from './Route.svelte';
 import { lastSubmenuPages } from './stores/breadcrumb';
 import { navigationRegistry } from './stores/navigation/navigation-registry';
 import SubmenuNavigation from './SubmenuNavigation.svelte';
-  import NetworkDetails from './lib/network/NetworkDetails.svelte';
 
 router.mode.memory();
 
--- packages/renderer/src/lib/network/NetworkDetails.svelte
+++ packages/renderer/src/lib/network/NetworkDetails.svelte
@@ -2,14 +2,17 @@
 import { StatusIcon, Tab } from '@podman-desktop/ui-svelte';
 import { router } from 'tinro';
 
+import { networksListInfo } from '/@/stores/networks';
+
 import Route from '../../Route.svelte';
 import VolumeIcon from '../images/VolumeIcon.svelte';
 import DetailsPage from '../ui/DetailsPage.svelte';
 import { getTabUrl, isTabSelected } from '../ui/Util';
-import VolumeDetailsSummary from '././VolumeDetailsSummary.svelte';
+import { NetworkUtils } from './network-utils';
+import NetworkActions from './NetworkActions.svelte';
+import NetworkDetailsInspect from './NetworkDetailsInspect.svelte';
+import NetworkDetailsSummary from './NetworkDetailsSummary.svelte';
 import type { NetworkInfoUI } from './NetworkInfoUI';
-import VolumeDetailsInspect from './VolumeDetailsInspect.svelte';
-import type { VolumeInfoUI } from './VolumeInfoUI';
 
 interface Props {
   networkName: string;
@@ -18,13 +21,13 @@ interface Props {
 
 let { networkName, engineId }: Props = $props();
 
-let network: NetworkInfoUI;
-let detailsPage: DetailsPage;
+let network: NetworkInfoUI | undefined = $state();
+let detailsPage: DetailsPage | undefined = $state();
 
 const networkUtils = new NetworkUtils();
 
 let matchingNetwork = $derived(
-  $networkList.find(network => network.name === networkName && network.engineId === engineId),
+  $networksListInfo.find(network => network.Name === networkName && network.engineId === engineId),
 );
 
 $effect(() => {
@@ -39,22 +42,26 @@ $effect(() => {
 {#if network}
   <DetailsPage title={network.name} subtitle={network.shortId} bind:this={detailsPage}>
     {#snippet iconSnippet()}
-      <StatusIcon icon={VolumeIcon} size={24} status={network.status} />
+      <StatusIcon icon={VolumeIcon} size={24} status={network?.status} />
     {/snippet}
     {#snippet actionsSnippet()}
-      <NetworkActions network={network} detailed={true} on:update={(): VolumeInfoUI => (network = network)} />
+      {#if network}
+        <NetworkActions network={network} detailed={true} />
+      {/if}
     {/snippet}
     {#snippet tabsSnippet()}
       <Tab title="Summary" selected={isTabSelected($router.path, 'summary')} url={getTabUrl($router.path, 'summary')} />
       <Tab title="Inspect" selected={isTabSelected($router.path, 'inspect')} url={getTabUrl($router.path, 'inspect')} />
     {/snippet}
     {#snippet contentSnippet()}
-      <Route path="/summary" breadcrumb="Summary" navigationHint="tab">
-        <VolumeDetailsSummary volume={volume} />
-      </Route>
-      <Route path="/inspect" breadcrumb="Inspect" navigationHint="tab">
-        <VolumeDetailsInspect volume={volume} />
-      </Route>
+      {#if network}
+        <Route path="/summary" breadcrumb="Summary" navigationHint="tab">
+          <NetworkDetailsSummary network={network} />
+        </Route>
+        <Route path="/inspect" breadcrumb="Inspect" navigationHint="tab">
+         <NetworkDetailsInspect network={network} />
+        </Route>
+      {/if}
     {/snippet}
   </DetailsPage>
 {/if}
