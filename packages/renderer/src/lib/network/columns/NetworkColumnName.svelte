35dfb2fe7d9b43d8cae66dedbbb450c5c1f79263
Sonia Sandler <ssandler@redhat.com>
2025-10-24 17:05:08 -0400

    chore: update after rebase Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/renderer/src/lib/network/columns/NetworkColumnName.svelte
+++ packages/renderer/src/lib/network/columns/NetworkColumnName.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+import { router } from 'tinro';
+
 import type { NetworkInfoUI } from '../NetworkInfoUI';
 
 interface Props {
@@ -6,10 +8,14 @@ interface Props {
 }
 
 let { object }: Props = $props();
+
+function openDetailsNetwork(network: NetworkInfoUI): void {
+  router.goto(`/networks/${encodeURI(network.name)}/${encodeURI(network.engineId)}/summary`);
+}
 </script>
 
 <button
   class="hover:cursor-pointer flex text-[var(--pd-table-body-text-highlight)] max-w-full overflow-hidden text-ellipsis"
-  >
+  onclick={(): void => openDetailsNetwork(object)}>
   {object.name}
 </button>
