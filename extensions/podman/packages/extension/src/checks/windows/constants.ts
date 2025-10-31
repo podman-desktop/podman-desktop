643a40c5479319f5cc6729f4cb2cd44429ba4ae7
Simon Rey <51708585+simonrey1@users.noreply.github.com>
2025-10-31 10:05:54 +0100

    refactor(extension/podman): move url and title of doc to constants
    Signed-off-by: Simon Rey <51708585+simonrey1@users.noreply.github.com>

---
+++ extensions/podman/packages/extension/src/checks/windows/constants.ts
@@ -0,0 +1,4 @@
+export const docLinksHyperV = {
+  url: 'https://learn.microsoft.com/en-us/virtualization/hyper-v-on-windows/quick-start/enable-hyper-v',
+  title: 'Hyper-V Manual Installation Steps',
+};
