50d4d8861986cdf333479299b83f9de82a47df90
Charlie Drage <charlie@charliedrage.com>
2025-11-06 08:51:05 -0500

    chore: when retrieving a configuration value, check locked and managed-by values Signed-off-by: Charlie Drage <charlie@charliedrage.com>

---
+++ packages/main/src/plugin/lock-configuration.ts
@@ -0,0 +1,60 @@
+/**********************************************************************
+ * Copyright (C) 2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import {
+  CONFIGURATION_SYSTEM_MANAGED_DEFAULTS_SCOPE,
+  CONFIGURATION_SYSTEM_MANAGED_LOCKED_SCOPE,
+} from '/@api/configuration/constants.js';
+
+/**
+ * Handles any "locked" configuration values managed by the managed-by configuration scopes.
+ */
+export class LockConfiguration {
+  constructor(private configurationValues: Map<string, { [key: string]: unknown }>) {}
+
+  /**
+   * Checks if a config key is locked and returns its managed value instead
+   * of the user-defined one.
+   *
+   * @param localKey - Configuration key to check
+   * @returns return the managed value if locked, undefined otherwise
+   */
+  get<T>(localKey: string): T | undefined {
+    const lockedConfig = this.configurationValues.get(CONFIGURATION_SYSTEM_MANAGED_LOCKED_SCOPE);
+
+    // Bail early if there's no locked config or it's malformed
+    if (!lockedConfig?.['locked'] || !Array.isArray(lockedConfig['locked'])) {
+      return undefined;
+    }
+
+    const lockedKeys = lockedConfig['locked'] as string[];
+
+    // Bail early if this key isn't in the locked list
+    if (!lockedKeys.includes(localKey)) {
+      return undefined;
+    }
+
+    // This key IS locked - grab the managed default value for it
+    const managedDefaults = this.configurationValues.get(CONFIGURATION_SYSTEM_MANAGED_DEFAULTS_SCOPE);
+    if (managedDefaults?.[localKey] !== undefined) {
+      return managedDefaults[localKey] as T;
+    }
+
+    return undefined;
+  }
+}
