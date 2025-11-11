827d16b7beb08cc0c9b8eee627449c2a66deae8b
Denis Golovin <dgolovin@redhat.com>
2025-11-11 00:23:20 -0800

    feat: provide `Apply YAML`  page for kubernetes dashboard resources
    This commit introduces a page to apply kubernetes YAML to
    a cluster and namespase form the selected kubernetes context.
    
    The page provides UI to:
    1. Select Kubernetes context different from current context
    2. Select .yaml or .yml file to apply
    3. Provide custom YAML content to apply
    
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>
    Co-authored-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    Co-authored-by: Simon Rey <51708585+simonrey1@users.noreply.github.com>

--- packages/renderer/src/lib/kube/KubeApplyYAMLButton.svelte
+++ packages/renderer/src/lib/kube/KubeApplyYAMLButton.svelte
@@ -1,79 +1,8 @@
 <script lang="ts">
-import type { KubernetesObject } from '@kubernetes/client-node';
 import { Button } from '@podman-desktop/ui-svelte';
+import { router } from 'tinro';
 
 import SolidKubeIcon from '../images/SolidKubeIcon.svelte';
-
-let inProgress = false;
-
-async function kubeApply(): Promise<void> {
-  let contextName = await window.kubernetesGetCurrentContextName();
-  if (!contextName) {
-    return;
-  }
-
-  const result = await window.openDialog({
-    title: 'Select a .yaml file to apply',
-    selectors: ['openFile', 'multiSelections'],
-    filters: [
-      {
-        name: 'YAML files',
-        extensions: ['yaml', 'yml', 'YAML', 'YML'],
-      },
-    ],
-  });
-
-  if (!result || result.length === 0) {
-    return;
-  }
-
-  inProgress = true;
-  try {
-    const namespace = await window.kubernetesGetCurrentNamespace();
-    let objects: KubernetesObject[] = await window.kubernetesApplyResourcesFromFile(contextName, result, namespace);
-    if (objects.length === 0) {
-      await window.showMessageBox({
-        title: 'Kubernetes',
-        type: 'warning',
-        message: `No resource(s) were applied.`,
-        buttons: ['OK'],
-      });
-    } else if (objects.length === 1) {
-      await window.showMessageBox({
-        title: 'Kubernetes',
-        type: 'info',
-        message: `Successfully applied 1 ${objects[0].kind ?? 'unknown resource'}.`,
-        buttons: ['OK'],
-      });
-    } else {
-      const counts = objects.reduce(
-        (acc, obj) => {
-          acc[obj.kind ?? 'unknown'] = (acc[obj.kind ?? 'unknown'] || 0) + 1;
-          return acc;
-        },
-        {} as Record<string, number>,
-      );
-      const resources = Object.entries(counts)
-        .map(obj => `${obj[1]} ${obj[0]}`)
-        .join(', ');
-
-      await window.showMessageBox({
-        title: 'Kubernetes',
-        type: 'info',
-        message: `Successfully applied ${objects.length} resources (${resources}).`,
-        buttons: ['OK'],
-      });
-    }
-  } catch (error) {
-    await window.showMessageBox({
-      title: 'Kubernetes',
-      type: 'error',
-      message: 'Could not apply Kubernetes YAML: ' + error,
-      buttons: ['OK'],
-    });
-  }
-  inProgress = false;
-}
 </script>
 
-<Button on:click={kubeApply} title="Apply YAML" icon={SolidKubeIcon} inProgress={inProgress}>Apply YAML</Button>
+<Button on:click={(): void => router.goto('/kubernetes/apply')} title="Apply YAML" icon={SolidKubeIcon}>Apply YAML</Button>
