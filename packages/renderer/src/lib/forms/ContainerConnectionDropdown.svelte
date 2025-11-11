f5410d419801d8487d354f3a0bee2a04e209cc1e
Denis Golovin <dgolovin@redhat.com>
2025-11-11 00:23:20 -0800

    feat: apply kube yaml for kubernetes pages
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- packages/renderer/src/lib/forms/ContainerConnectionDropdown.svelte
+++ packages/renderer/src/lib/forms/ContainerConnectionDropdown.svelte
@@ -53,8 +53,14 @@ function handleChange(nValue: unknown): void {
   disabled={disabled}
   value={selected}
   onChange={handleChange}
-  options={Array.from(items.entries()).map(([key, connection]) => ({
-            label: connection.name,
-            value: key,
-          }))}>
+  options={[{
+            label: 'Connection 1',
+            value: '1',
+          }, {
+            label: 'Connection 2',
+            value: '2',
+          }, {
+            label: 'Connection 3',
+            value: '3',
+          }]}>
 </Dropdown>
