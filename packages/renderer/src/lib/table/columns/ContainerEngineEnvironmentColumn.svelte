c8ea5d966bdf842399b7cace4bfadaf80217fc37
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-11-24 15:28:54 +0100

    feat(renderer): nicely display container engine connection name in environment column
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/table/columns/ContainerEngineEnvironmentColumn.svelte
+++ packages/renderer/src/lib/table/columns/ContainerEngineEnvironmentColumn.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
-import ProviderInfo from '/@/lib/ui/ProviderInfo.svelte';
+import Label from '/@/lib/ui/Label.svelte';
+import ProviderInfoCircle from '/@/lib/ui/ProviderInfoCircle.svelte';
+import { providerInfos } from '/@/stores/providers';
 
 interface Props {
   object: {
     engineId: string;
-    engineName: string;
   };
 }
 
 let { object }: Props = $props();
+
+const [providerId, connectionName] = $derived(object.engineId.split('.'));
+
+const connection = $derived(
+  $providerInfos
+    .find(provider => provider.id === providerId)
+    ?.containerConnections?.find(({ name }) => name === connectionName),
+);
 </script>
 
-<ProviderInfo provider={object.engineName} context={object.engineId} />
+<Label tip={connection?.endpoint.socketPath} name={connection?.displayName}>
+  <ProviderInfoCircle type="podman" />
+</Label>
