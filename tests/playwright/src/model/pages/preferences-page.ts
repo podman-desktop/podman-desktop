7c45a2e3dd0012d7228205983fad29946bc21096
Tibor Dancs <tdancs@redhat.com>
2025-12-02 18:19:55 +0100

    chore(test): adding basic smoke test for settings.json preferences
    Signed-off-by: Tibor Dancs <tdancs@redhat.com>

--- tests/playwright/src/model/pages/preferences-page.ts
+++ tests/playwright/src/model/pages/preferences-page.ts
@@ -19,18 +19,69 @@
 import type { Locator, Page } from '@playwright/test';
 import test, { expect as playExpect } from '@playwright/test';
 
+import { delay } from '/@/utility/wait';
+
 import { SettingsPage } from './settings-page';
 
 export class PreferencesPage extends SettingsPage {
   readonly heading: Locator;
+  readonly searchbar: Locator;
   readonly kubePathInput: Locator;
 
   constructor(page: Page) {
     super(page, 'Preferences');
     this.heading = this.header.getByLabel('Title', { exact: true });
+    this.searchbar = this.header.getByLabel('search preferences');
     this.kubePathInput = this.content.getByLabel(
       'Path to the Kubeconfig file for accessing clusters. (Default is usually ~/.kube/config)',
     );
+  }
+
+  getPreferenceRowByName(name: string): Locator {
+    const preferenceLocator = this.content.getByText(name, { exact: true }).last();
+    return preferenceLocator.locator('xpath=ancestor::div[@class="flex flex-row justify-between"]');
+  }
+
+  async isPreferenceManaged(name: string): Promise<boolean> {
+    const preferenceRow = this.getPreferenceRowByName(name);
+    const managedIndicator = await preferenceRow.getByText('Managed').all();
+    return managedIndicator.length > 0;
+  }
+
+  async resetPreference(preferenceRow: Locator): Promise<void> {
+    const resetButton = preferenceRow.getByRole('button', { name: 'Reset to default value' });
+    await playExpect(resetButton).toBeVisible();
+    await resetButton.click();
+    await delay(1000); // wait for the preference to be applied
+  }
+
+  async getAppearancePreferenceValue(): Promise<string> {
+    const appearancePreferenceRow = this.getPreferenceRowByName('Appearance');
+    await playExpect(appearancePreferenceRow).toBeAttached();
+    await appearancePreferenceRow.scrollIntoViewIfNeeded();
+    await playExpect(appearancePreferenceRow).toBeVisible();
+
+    const preferenceInput = appearancePreferenceRow.getByLabel('hidden input');
+    await playExpect(preferenceInput).toBeAttached();
+    return await preferenceInput.inputValue();
+  }
+
+  async setAppearancePreference(value: string): Promise<void> {
+    const appearancePreferenceRow = this.getPreferenceRowByName('Appearance');
+    await playExpect(appearancePreferenceRow).toBeAttached();
+    await appearancePreferenceRow.scrollIntoViewIfNeeded();
+    await playExpect(appearancePreferenceRow).toBeVisible();
+
+    const selectionButton = appearancePreferenceRow.locator('button#input-standard-preferences\\.appearance');
+    await playExpect(selectionButton).toBeVisible();
+    await selectionButton.click();
+
+    const option = appearancePreferenceRow.getByRole('button', { name: value, exact: true });
+    await playExpect(option).toBeAttached();
+    await option.scrollIntoViewIfNeeded();
+    await playExpect(option).toBeVisible();
+    await option.click();
+    await delay(1000); // wait for the preference to be applied
   }
 
   async selectKubeFile(pathToKube: string): Promise<void> {
