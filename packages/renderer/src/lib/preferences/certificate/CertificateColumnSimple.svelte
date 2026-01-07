461882bbd49f5cd6bbc4f4c274b0978e28c647b1
Denis Golovin <dgolovin@redhat.com>
2026-01-07 01:20:48 -0800

    fix: update css var usage
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- packages/renderer/src/lib/preferences/certificate/CertificateColumnSimple.svelte
+++ packages/renderer/src/lib/preferences/certificate/CertificateColumnSimple.svelte
@@ -6,7 +6,7 @@ interface Props {
 let { object }: Props = $props();
 </script>
 
-<div title={object} class="text-[var(--pd-table-body-text-highlight) overflow-hidden text-ellipsis">
+<div title={object} class="text-(--pd-table-body-text-highlight) overflow-hidden text-ellipsis">
   {object}
 </div>
 
