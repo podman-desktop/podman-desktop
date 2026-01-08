32f418765f562dd013c4e93dd77536a06e904bb9
Denis Golovin <dgolovin@redhat.com>
2026-01-07 21:46:09 -0800

    fix: add missing ui elements: button, slide toggle and title
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- packages/renderer/src/lib/preferences/certificate/CertificateList.svelte
+++ packages/renderer/src/lib/preferences/certificate/CertificateList.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-import { FilteredEmptyScreen, NavPage, Table, TableColumn, TableRow } from '@podman-desktop/ui-svelte';
+import { Button, FilteredEmptyScreen, NavPage, Table, TableColumn, TableRow } from '@podman-desktop/ui-svelte';
 import { onDestroy, onMount } from 'svelte';
 import type { Unsubscriber } from 'svelte/store';
 
+import SlideToggle from '/@/lib/ui/SlideToggle.svelte';
 import { certificatesInfos, filtered, searchPattern } from '/@/stores/certificates';
 import type { CertificateInfo } from '/@api/certificate-info';
 
@@ -19,6 +20,7 @@ interface Props {
 }
 
 let { searchTerm = $bindable('') }: Props = $props();
+let checked = $state(true);
 $effect(() => {
   searchPattern.set(searchTerm);
 });
@@ -119,32 +121,67 @@ function key(item: CertificateInfoUI): string {
 function label(item: CertificateInfoUI): string {
   return getDisplayName(item);
 }
+
+function onChecked(state: boolean): void {
+  checked = state;
+}
 </script>
 
 <NavPage bind:searchTerm={searchTerm} title="Certificates">
+  {#snippet additionalActions()}
+    <Button class="mr-5">Synchronize into podman-machine-default</Button>
+  {/snippet}
   {#snippet content()}
-    <div class="flex min-w-full h-full">
-      {#if $certificatesInfos.length === 0}
-        <CertificateEmptyScreen />
-      {:else if certificates.length === 0}
-        {#if searchTerm}
-          <FilteredEmptyScreen icon={CertificateIcon} kind="certificates" bind:searchTerm={searchTerm} />
-        {:else}
+    <div class="flex flex-col px-2 py-2 w-full">
+      <div class="bg-(--pd-invert-content-card-bg) rounded-md mx-5"> 
+        <div class="flex flex-col px-2 py-2 w-full text-(--pd-invert-content-card-text) space-y-4">
+          <dif class="flex flex-row justify-between">
+            <div class="flex flex-col">
+              <div class="flex flex-row text-(--pd-invert-content-card-text)">
+                <div class="flex flex-row space-x-2 items-center">
+                  <span class="font-semibold">Synchronize Certificates</span>
+                </div>
+              </div>
+              <div class="pt-1 text-(--pd-invert-content-card-text) text-sm pr-2">Automatically synchronize certificates to all Podman machines.</div>        
+            </div>
+            <SlideToggle
+              id="sync-all-certificates"
+              name="Automatic Synchronize"
+              left
+              bind:checked={checked}
+              on:checked={(event): void => onChecked(event.detail)}
+              aria-label="Automatic Ceritficates Synchronization">
+              <span class="text-xs">{checked ? 'Enabled' : 'Disabled'}</span>
+            </SlideToggle>
+          </dif>
+        </div>
+      </div>
+      <div class="self-start text-(--pd-invert-content-card-text) mx-5 my-5">
+        Manage host-based certificates in Podman Desktop with automatic synchronization to your Podman Machine.
+      </div>
+      <div class="flex min-w-full h-full">
+        {#if $certificatesInfos.length === 0}
           <CertificateEmptyScreen />
+        {:else if certificates.length === 0}
+          {#if searchTerm}
+            <FilteredEmptyScreen icon={CertificateIcon} kind="certificates" bind:searchTerm={searchTerm} />
+          {:else}
+            <CertificateEmptyScreen />
+          {/if}
+        {:else}
+          <Table
+            kind="certificate"
+            data={certificates}
+            columns={columns}
+            row={row}
+            defaultSortColumn="Certificate Name"
+            key={key}
+            label={label}
+            enableLayoutConfiguration={true}
+            on:update={(): CertificateInfoUI[] => (certificates = certificates)}>
+          </Table>
         {/if}
-      {:else}
-        <Table
-          kind="certificate"
-          data={certificates}
-          columns={columns}
-          row={row}
-          defaultSortColumn="Certificate Name"
-          key={key}
-          label={label}
-          enableLayoutConfiguration={true}
-          on:update={(): CertificateInfoUI[] => (certificates = certificates)}>
-        </Table>
-      {/if}
+      </div>
     </div>
   {/snippet}
 </NavPage>
