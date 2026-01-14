eaac1a510a62692c71aba2cff643d6eba881c199
axel7083 <42176370+axel7083@users.noreply.github.com>
2026-01-14 07:29:57 +0100

    fix(ui): dropdown menu item sizing (#6)
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/ui/src/lib/dropdownMenu/DropDownMenuItem.svelte
+++ packages/ui/src/lib/dropdownMenu/DropDownMenuItem.svelte
@@ -25,9 +25,9 @@ const disabledClasses = 'text-[var(--pd-dropdown-disabled-item-text)] bg-[var(--
   <div class={`p-2.5 ${enabled ? enabledClasses : disabledClasses}`} role="none" onclick={onClick}>
     <span
       title={tooltip !== '' ? tooltip : title}
-      class="group flex items-center no-underline whitespace-nowrap"
+      class="group flex items-center no-underline whitespace-nowrap h-4"
       tabindex="-1">
-      <Icon class="h-4 w-4 text-md" icon={icon} />
+      <Icon class="w-4 text-md" icon={icon} />
       {#if title}<span class="ml-2">{title}</span>{/if}
     </span>
   </div>
