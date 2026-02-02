ff0c5a54d746b49bc25e8f947affaa65fbb27024
axel7083 <42176370+axel7083@users.noreply.github.com>
2026-02-02 13:17:11 +0100

    test: increase coverage of LinuxSocketCompatibility
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- extensions/podman/packages/extension/src/compatibility-mode/linux-socket-compatibility.spec.ts
+++ extensions/podman/packages/extension/src/compatibility-mode/linux-socket-compatibility.spec.ts
@@ -15,11 +15,15 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************/
+import { existsSync } from 'node:fs';
+
 import * as extensionApi from '@podman-desktop/api';
-import { beforeEach, expect, test, vi } from 'vitest';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
 
 import { getSocketCompatibility } from '/@/compatibility-mode/compatibility-mode';
 import { LinuxSocketCompatibility } from '/@/compatibility-mode/linux-socket-compatibility';
+
+vi.mock(import('node:fs'));
 
 beforeEach(() => {
   vi.resetAllMocks();
@@ -34,12 +38,62 @@ test('linux: compatibility mode pass', async () => {
   expect(() => getSocketCompatibility()).toBeTruthy();
 });
 
-// Fail when trying to use runSystemdCommand with a command that's not "enable" or "disable"
-test('linux: fail when trying to use runSystemdCommand with a command that is not "enable" or "disable"', async () => {
-  const socketCompatClass = new LinuxSocketCompatibility();
-  await expect(socketCompatClass.runSystemdCommand('start', 'enabled')).rejects.toThrowError(
-    'runSystemdCommand only accepts enable or disable as the command',
-  );
+describe('runSystemdCommand', () => {
+  // Fail when trying to use runSystemdCommand with a command that's not "enable" or "disable"
+  test('linux: fail when trying to use runSystemdCommand with a command that is not "enable" or "disable"', async () => {
+    const socketCompatClass = new LinuxSocketCompatibility();
+    await expect(socketCompatClass.runSystemdCommand('start', 'enabled')).rejects.toThrowError(
+      'runSystemdCommand only accepts enable or disable as the command',
+    );
+  });
+
+  test('systemctl error should show user error message', async () => {
+    vi.mocked(extensionApi.process.exec).mockRejectedValue('dummy');
+
+    const socketCompatClass = new LinuxSocketCompatibility();
+    await socketCompatClass.runSystemdCommand('enable', 'enable');
+
+    expect(extensionApi.window.showErrorMessage).toHaveBeenCalledExactlyOnceWith(
+      expect.stringMatching('Error running systemctl command: dummy'),
+      'OK',
+    );
+  });
+
+  test('enable command should ask user to create symlink', async () => {
+    vi.mocked(extensionApi.window.showInformationMessage).mockResolvedValue('Yes');
+
+    const socketCompatClass = new LinuxSocketCompatibility();
+    await socketCompatClass.runSystemdCommand('enable', 'enable');
+
+    expect(extensionApi.process.exec).toHaveBeenLastCalledWith('pkexec', [
+      'ln',
+      '-s',
+      '/run/podman/podman.sock',
+      '/var/run/docker.sock',
+    ]);
+  });
+
+  test('error in symlink creation should show user error message', async () => {
+    vi.mocked(extensionApi.window.showInformationMessage).mockResolvedValue('Yes');
+    vi.mocked(extensionApi.process.exec).mockImplementation(async (command): Promise<extensionApi.RunResult> => {
+      switch (command) {
+        case 'systemctl':
+          return { command, stdout: '', stderr: '' };
+        case 'pkexec':
+          throw new Error('dummy pkexec error');
+        default:
+          throw new Error(`unknown command ${command}`);
+      }
+    });
+
+    const socketCompatClass = new LinuxSocketCompatibility();
+    await socketCompatClass.runSystemdCommand('enable', 'enable');
+
+    expect(extensionApi.window.showErrorMessage).toHaveBeenCalledExactlyOnceWith(
+      'Error creating symlink: Error: dummy pkexec error',
+      'OK',
+    );
+  });
 });
 
 test('linux: pass enabling when systemctl command exists', async () => {
@@ -50,4 +104,54 @@ test('linux: pass enabling when systemctl command exists', async () => {
   // Expect enable() to pass since systemctl command exists
   await expect(socketCompatClass.enable()).resolves.toBeUndefined();
   expect(extensionApi.window.showErrorMessage).not.toHaveBeenCalled();
+});
+
+describe('tooltip', () => {
+  test.each<{
+    enabled: boolean;
+    text: string;
+  }>([
+    {
+      enabled: true,
+      text: 'Disable Linux Docker socket compatibility for Podman.',
+    },
+    {
+      enabled: false,
+      text: 'Enable Linux Docker socket compatibility for Podman.',
+    },
+  ])('text should be $text when isEnabled is $enabled', ({ enabled, text }) => {
+    // Mock that the binary is found
+    const socketCompatClass = new LinuxSocketCompatibility();
+
+    vi.spyOn(socketCompatClass, 'isEnabled').mockReturnValue(enabled);
+
+    expect(socketCompatClass.tooltipText()).toEqual(text);
+  });
+});
+
+describe('isEnabled', () => {
+  test('should return true if corresponding file exists', () => {
+    vi.mocked(existsSync).mockReturnValue(true);
+
+    const socketCompatClass = new LinuxSocketCompatibility();
+
+    expect(socketCompatClass.isEnabled()).toBeTruthy();
+    expect(existsSync).toHaveBeenCalledExactlyOnceWith(`/etc/systemd/system/socket.target.wants/podman.socket`);
+  });
+
+  test('should return false if corresponding file does not exist', () => {
+    vi.mocked(existsSync).mockReturnValue(false);
+
+    const socketCompatClass = new LinuxSocketCompatibility();
+
+    expect(socketCompatClass.isEnabled()).toBeFalsy();
+    expect(existsSync).toHaveBeenCalledExactlyOnceWith(`/etc/systemd/system/socket.target.wants/podman.socket`);
+  });
+});
+
+test('disable exec systemctl disable', async () => {
+  const socketCompatClass = new LinuxSocketCompatibility();
+
+  await socketCompatClass.disable();
+  expect(extensionApi.process.exec).toHaveBeenCalledExactlyOnceWith('systemctl', ['disable', '--now', 'podman.socket']);
 });
