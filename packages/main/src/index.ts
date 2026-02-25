098a48461ac9708aa8aaa186c8b3f84ce2176c1f
Gregory McNutt <amcnutt1996@gmail.com>
2026-02-25 15:25:46 -0500

    fix(ui): updated based on comments in PR Signed-off-by: Gregory McNutt <amcnutt1996@gmail.com>

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (C) 2022-2026 Red Hat, Inc.
+ * Copyright (C) 2026 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
-import { console } from 'node:inspector';
 
 import type { Event } from '@podman-desktop/core-api';
 import { app, ipcMain, Menu, Tray } from 'electron';
