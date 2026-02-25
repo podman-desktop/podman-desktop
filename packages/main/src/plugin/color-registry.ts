ce30e6b858efcdc2eda1f098ed6c33006cdd8de6
Florent Benoit <fbenoit@redhat.com>
2026-02-25 07:21:15 +0100

    chore: keep relative import for color-registry Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/main/src/plugin/color-registry.ts
+++ packages/main/src/plugin/color-registry.ts
@@ -22,10 +22,13 @@ import type { ApiSenderType } from '@podman-desktop/core-api/api-sender';
 import { AppearanceSettings } from '@podman-desktop/core-api/appearance';
 
 import type { AnalyzedExtension } from '/@/plugin/extension/extension-analyzer.js';
-import { isWindows } from '/@/util.js';
 
 // eslint-disable-next-line no-restricted-imports
 import tailwindColorPalette from '../../../../tailwind-color-palette.json' with { type: 'json' };
+// use relative path as this file is embedded in the website build to generate css
+// and then type alias won't resolve it
+// eslint-disable-next-line no-restricted-imports
+import { isWindows } from '../util.js';
 import { ColorBuilder } from './color-builder.js';
 import { colorPaletteHelper } from './color-palette-helper.js';
 import type { ConfigurationRegistry } from './configuration-registry.js';
