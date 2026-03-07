d67126a05b1ee4712a4184cc2937ebc8668e997a
sheikhlimon <sheikhlimon404@gmail.com>
2026-03-07 20:26:36 +0600

    feat(container-registry): make provider timeout configurable in settings
    Signed-off-by: sheikhlimon <sheikhlimon404@gmail.com>

--- packages/main/src/plugin/index.ts
+++ packages/main/src/plugin/index.ts
@@ -617,6 +617,7 @@ export class PluginSystem {
     Object.freeze(notifications);
     const kubeGeneratorRegistry = container.get<KubeGeneratorRegistry>(KubeGeneratorRegistry);
     const containerProviderRegistry = container.get<ContainerProviderRegistry>(ContainerProviderRegistry);
+    containerProviderRegistry.init();
     kubeGeneratorRegistry.registerDefaultKubeGenerator({
       name: 'PodmanKube',
       types: ['Compose', 'Container', 'Pod'],
