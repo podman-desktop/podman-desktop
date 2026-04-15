2567d737d98e3502ed77cce26179af4781be79dc
Sonia Sandler <66797193+SoniaSandler@users.noreply.github.com>
2026-04-15 11:51:38 -0400

    chore: use product name for title bar text (#16644)
    * chore: use product name for TitleBar text
    Signed-off-by: Sonia Sandler <ssandler@redhat.com>
    
    * chore: adjust product.json property name
    Signed-off-by: Sonia Sandler <ssandler@redhat.com>
    
    * chore: undo commandPalette change
    Signed-off-by: Sonia Sandler <ssandler@redhat.com>
    
    * chore: apply reviews
    Signed-off-by: Sonia Sandler <ssandler@redhat.com>
    (cherry picked from commit 89c3aebdaa8107f43237cfca462287002817d7d7)

--- packages/main/src/plugin/index.ts
+++ packages/main/src/plugin/index.ts
@@ -1757,6 +1757,10 @@ export class PluginSystem {
       return podmanDesktopUpdater.getReleaseNotes();
     });
 
+    this.ipcHandle('app:getTitleBarText', async (_listener): Promise<string> => {
+      return product.name;
+    });
+
     this.ipcHandle('provider-registry:getProviderInfos', async (): Promise<ProviderInfo[]> => {
       return providerRegistry.getProviderInfos();
     });
