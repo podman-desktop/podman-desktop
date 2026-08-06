3ccaf732fcf38b5a9b03603b9ed7e0351ab1d4f8
Evzen Gasta <evzen.ml@seznam.cz>
2026-08-06 11:24:02 +0200

    refactor(Icon): migrated Icon components to svelte5
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>

--- packages/renderer/src/lib/images/VolumeIcon.svelte
+++ packages/renderer/src/lib/images/VolumeIcon.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-export let size = '40';
-export let solid = false;
+interface Props {
+  size?: string;
+  solid?: boolean;
+  class?: string;
+  style?: string;
+}
+let { size = '40', solid = false, class: className, style: styleName }: Props = $props();
 </script>
 
 <svg
   width={size}
   height={size}
-  class={$$props.class}
-  style={$$props.style}
+  class={className}
+  style={styleName}
   viewBox="0.926 0.926 4.498 4.498"
   version="1.1"
   xml:space="preserve"
