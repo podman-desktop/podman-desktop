1c23afbc7f6dc760c65a29e70d755686151daaf2
Sonia Sandler <ssandler@redhat.com>
2026-09-07 19:18:12 -0400

    chore: apply changes Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/renderer/src/lib/dialogs/MessageBox.svelte
+++ packages/renderer/src/lib/dialogs/MessageBox.svelte
@@ -47,7 +47,7 @@ const showMessageBoxCallback = (messageBoxParameter: unknown): void => {
   buttonOrder = Array.from(buttonsType, (value, index) => index);
 
   // use the provided cancel id, otherwise try to find a button labelled 'cancel'
-  if (options?.cancelId !== undefined) {
+  if (options?.cancelId) {
     cancelId = options.cancelId;
   } else {
     cancelId = buttonsType.findIndex(b => {
