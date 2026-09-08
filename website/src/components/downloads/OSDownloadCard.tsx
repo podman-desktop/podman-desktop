b416990bc1c149a7d8ff8e0376a89cadb5360583
Sonia Sandler <ssandler@redhat.com>
2026-09-08 12:06:36 -0400

    chore: add telemetry for OS card expansion Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- website/src/components/downloads/OSDownloadCard.tsx
+++ website/src/components/downloads/OSDownloadCard.tsx
@@ -19,7 +19,7 @@
 import type { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { faCheck, faChevronDown, faDownload, faPaste, faTerminal } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { TelemetryLink } from '@site/src/components/TelemetryLink';
+import { sendGoatCounterEvent, TelemetryLink } from '@site/src/components/TelemetryLink';
 import React from 'react';
 
 function CopyButton({ onCopy }: { readonly onCopy: () => Promise<void> }): JSX.Element {
@@ -97,6 +97,9 @@ export function OSDownloadCard({
 
   const toggleExpanded = (): void => {
     setExpanded(value => !value);
+    if (expanded) {
+      sendGoatCounterEvent('download', `expand-${osName}`);
+    }
   };
 
   return (
