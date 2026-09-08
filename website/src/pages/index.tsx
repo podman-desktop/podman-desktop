ee5df3c61e5644089e7f1677814ce3334b3f847c
Sonia Sandler <ssandler@redhat.com>
2026-09-08 11:19:44 -0400

    fix: red hat vendor card width on small screen Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -435,7 +435,7 @@ function Pods(): JSX.Element {
 function VendorCards(): JSX.Element {
   return (
     <section className="text-charcoal-300 dark:text-white body-font py-24 bg-white dark:bg-black bg-gradient-to-tr from-purple-200/60 dark:from-purple-900/40 to-transparent">
-      <div className="container mx-auto flex flex-col px-5 lg:">
+      <div className="container mx-auto flex flex-col px-5">
         <h2 className="text-4xl/[1.5] font-bold mb-4 text-charcoal-300 dark:text-white">Enterprise support</h2>
         <p className="text-base md:text-lg text-charcoal-300 dark:text-gray-300 mb-10 w-full">
           Need enterprise-grade support for production? Explore commercial support options with long-term maintenance
@@ -448,7 +448,7 @@ function VendorCards(): JSX.Element {
             description="Red Hat offers the Red Hat Build of Podman Desktop with enterprise support, long-term maintenance, and production-ready builds."
             logo="/img/redhat-logo.svg"
             learnMore="https://red.ht/redhatbuildofpodmandesktop"
-            addClass="col-span-2"
+            addClass="lg:col-span-2"
           />
           <AddVendorCard />
         </div>
